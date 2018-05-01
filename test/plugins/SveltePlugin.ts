7d2bf8c44604d07dcbe81a63101cfd6b86e31626
James Birtles <jameshbirtles@gmail.com>
2018-05-01 20:13:00 +0100

    fix test

--- test/plugins/SveltePlugin.ts
+++ test/plugins/SveltePlugin.ts
@@ -4,13 +4,13 @@ import { SvelteDocument } from '../../src/lib/documents/SvelteDocument';
 import { Diagnostic, Range, DiagnosticSeverity } from '../../src/api';
 
 describe('Svelte Plugin', () => {
-    it('provides diagnostic warnings', () => {
+    it('provides diagnostic warnings', async () => {
         const plugin = new SveltePlugin();
         const document = new SvelteDocument(
             'file:///hello.html',
             '<h1>Hello, world!</h1>\n<img src="hello.png">',
         );
-        const diagnostics = plugin.getDiagnostics(document);
+        const diagnostics = await plugin.getDiagnostics(document);
         const diagnostic = Diagnostic.create(
             Range.create(1, 0, 1, 21),
             'A11y: <img> element should have an alt attribute',
@@ -22,10 +22,10 @@ describe('Svelte Plugin', () => {
         assert.deepStrictEqual(diagnostics, [diagnostic]);
     });
 
-    it('provides diagnostic errors', () => {
+    it('provides diagnostic errors', async () => {
         const plugin = new SveltePlugin();
         const document = new SvelteDocument('file:///hello.html', '<div bind:whatever></div>');
-        const diagnostics = plugin.getDiagnostics(document);
+        const diagnostics = await plugin.getDiagnostics(document);
         const diagnostic = Diagnostic.create(
             Range.create(0, 5, 0, 18),
             "'whatever' is not a valid binding",
