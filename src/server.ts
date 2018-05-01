d86c1aa617de8e9b01e7050fc859455b47e56a26
James Birtles <jameshbirtles@gmail.com>
2018-05-01 19:48:08 +0100

    add bin

--- src/server.ts
+++ src/server.ts
@@ -12,48 +12,53 @@ import { CSSPlugin } from './plugins/CSSPlugin';
 import { wrapFragmentPlugin } from './api/wrapFragmentPlugin';
 import { TypeScriptPlugin } from './plugins/TypeScriptPlugin';
 
-const connection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+function startServer() {
+    const connection = createConnection(
+        new IPCMessageReader(process),
+        new IPCMessageWriter(process),
+    );
 
-const manager = new DocumentManager(
-    textDocument => new SvelteDocument(textDocument.uri, textDocument.text),
-);
+    const manager = new DocumentManager(
+        textDocument => new SvelteDocument(textDocument.uri, textDocument.text),
+    );
 
-manager.register(new SveltePlugin());
-manager.register(new HTMLPlugin());
-manager.register(wrapFragmentPlugin(new CSSPlugin(), CSSPlugin.matchFragment));
-manager.register(wrapFragmentPlugin(new TypeScriptPlugin(), TypeScriptPlugin.matchFragment));
+    manager.register(new SveltePlugin());
+    manager.register(new HTMLPlugin());
+    manager.register(wrapFragmentPlugin(new CSSPlugin(), CSSPlugin.matchFragment));
+    manager.register(wrapFragmentPlugin(new TypeScriptPlugin(), TypeScriptPlugin.matchFragment));
 
-connection.onInitialize(evt => {
-    return {
-        capabilities: {
-            textDocumentSync: {
-                openClose: true,
-                change: TextDocumentSyncKind.Incremental,
+    connection.onInitialize(evt => {
+        return {
+            capabilities: {
+                textDocumentSync: {
+                    openClose: true,
+                    change: TextDocumentSyncKind.Incremental,
+                },
+                hoverProvider: manager.supports('doHover'),
+                completionProvider: {
+                    triggerCharacters: ['<'],
+                },
+                documentFormattingProvider: true,
             },
-            hoverProvider: manager.supports('doHover'),
-            completionProvider: {
-                triggerCharacters: ['<'],
-            },
-            documentFormattingProvider: true,
-        },
-    };
-});
-
-connection.onDidOpenTextDocument(evt => manager.openDocument(evt.textDocument));
-connection.onDidCloseTextDocument(evt => manager.closeDocument(evt.textDocument));
-connection.onDidChangeTextDocument(evt =>
-    manager.updateDocument(evt.textDocument, evt.contentChanges),
-);
-connection.onHover(evt => manager.doHover(evt.textDocument, evt.position));
-connection.onCompletion(evt => manager.getCompletions(evt.textDocument, evt.position));
-connection.onDocumentFormatting(evt => manager.formatDocument(evt.textDocument));
-
-manager.on('documentChange', async document => {
-    const diagnostics = await manager.getDiagnostics({ uri: document.getURL() });
-    connection.sendDiagnostics({
-        uri: document.getURL(),
-        diagnostics,
+        };
     });
-});
 
-connection.listen();
+    connection.onDidOpenTextDocument(evt => manager.openDocument(evt.textDocument));
+    connection.onDidCloseTextDocument(evt => manager.closeDocument(evt.textDocument));
+    connection.onDidChangeTextDocument(evt =>
+        manager.updateDocument(evt.textDocument, evt.contentChanges),
+    );
+    connection.onHover(evt => manager.doHover(evt.textDocument, evt.position));
+    connection.onCompletion(evt => manager.getCompletions(evt.textDocument, evt.position));
+    connection.onDocumentFormatting(evt => manager.formatDocument(evt.textDocument));
+
+    manager.on('documentChange', async document => {
+        const diagnostics = await manager.getDiagnostics({ uri: document.getURL() });
+        connection.sendDiagnostics({
+            uri: document.getURL(),
+            diagnostics,
+        });
+    });
+
+    connection.listen();
+}
