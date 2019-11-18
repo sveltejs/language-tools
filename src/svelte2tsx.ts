c92067bb1e2f6428f4d1675454bb1638ebf074ed
halfnelson <dpershouse@gmail.com>
2019-11-18 22:36:33 +1000

    detect $$props in script

--- src/svelte2tsx.ts
+++ src/svelte2tsx.ts
@@ -2,7 +2,8 @@ import MagicString from 'magic-string'
 import { parseHtmlx } from './parser';
 import { convertHtmlxToJsx } from './htmlxtojsx';
 import { Node } from 'svelte/compiler'
-import { createSourceFile, ScriptTarget, ScriptKind, SourceFile, SyntaxKind, VariableStatement, Identifier, FunctionDeclaration, BindingName, ExportDeclaration, ScriptSnapshot, LabeledStatement, ExpressionStatement, BinaryExpression } from 'typescript'
+//import { createSourceFile, ScriptTarget, ScriptKind, SourceFile, SyntaxKind, VariableStatement, Identifier, FunctionDeclaration, BindingName, ExportDeclaration, ScriptSnapshot, LabeledStatement, ExpressionStatement, BinaryExpression, Statement } from 'typescript'
+import * as ts from 'typescript';
 
 function AttributeValueAsJsExpression(htmlx: string, attr: Node): string {
     if (attr.value.length == 0) return "''"; //wut?
@@ -34,152 +35,6 @@ function AttributeValueAsJsExpression(htmlx: string, attr: Node): string {
     })
     let valuesAsStringTemplate = "`" + valueParts.join("") + "`";
     return valuesAsStringTemplate;
-}
-
-
-
-
-
-function processImports(str: MagicString, tsAst: SourceFile, astOffset: number, target: number) {
-    for (var st of tsAst.statements) {
-        if (st.kind == SyntaxKind.ImportDeclaration) {
-            str.move(st.pos+astOffset, st.end+astOffset,target);
-            str.overwrite(st.end+astOffset-1, st.end+astOffset, '"\n')
-        }
-    }
-}
-
-
-
-function declareImplictReactiveVariables(declaredNames: string[], str: MagicString, tsAst: SourceFile, astOffset: number) {
-    for (let le of tsAst.statements) {
-        if (le.kind != SyntaxKind.LabeledStatement) continue;
-        let ls = le as LabeledStatement;
-        if (ls.label.text != "$") continue;
-        if (!ls.statement || ls.statement.kind != SyntaxKind.ExpressionStatement) continue;
-        let es = ls.statement as ExpressionStatement;
-        if (!es.expression || es.expression.kind != SyntaxKind.BinaryExpression) continue;
-        let be = es.expression as BinaryExpression;
-        if (be.operatorToken.kind != SyntaxKind.EqualsToken
-            || be.left.kind != SyntaxKind.Identifier) continue;
-
-        let ident = be.left as Identifier;
-        //are we already declared?
-        if (declaredNames.find(n => ident.text == n)) continue;
-        //add a declaration
-        str.prependRight(ls.pos + astOffset + 1, `;let ${ident.text}; `)
-    }
-}
-
-function replaceExports(str: MagicString, tsAst: SourceFile, astOffset: number) {
-    //track a as b exports
-    let exportedNames = new Map<string, string>();
-    let declaredNames: string[] = [];
-
-    const addDeclaredName = (name: BindingName) => {
-        if (name.kind == SyntaxKind.Identifier) {
-            declaredNames.push(name.text);
-        }
-    }
-
-    const addExport = (name: BindingName, target: BindingName = null) => {
-        if (name.kind != SyntaxKind.Identifier) {
-            throw Error("export source kind not supported " + name)
-        }
-        if (target && target.kind != SyntaxKind.Identifier) {
-            throw Error("export target kind not supported " + target)
-        }
-        exportedNames.set(name.text, target ? (target as Identifier).text : null);
-    }
-
-    const removeExport = (start: number, end: number) => {
-        let exportStart = str.original.indexOf("export", start+astOffset);
-        let exportEnd = exportStart + "export".length;
-        str.remove(exportStart, exportEnd);
-    }
-
-    let statements = tsAst.statements;
-
-    for (let s of statements) {
-        if (s.kind == SyntaxKind.VariableStatement) {
-            let vs = s as VariableStatement;
-            let exportModifier = vs.modifiers
-                ? vs.modifiers.find(x => x.kind == SyntaxKind.ExportKeyword)
-                : null;
-            if (exportModifier) {
-                removeExport(exportModifier.pos, exportModifier.end);
-            }
-            for (let v of vs.declarationList.declarations) {
-                if (exportModifier) {
-                    addExport(v.name);
-                }
-                addDeclaredName(v.name);
-            }
-        }
-
-        if (s.kind == SyntaxKind.FunctionDeclaration) {
-            let fd = s as FunctionDeclaration;
-            if (fd.modifiers) {
-                let exportModifier = fd.modifiers.find(x => x.kind == SyntaxKind.ExportKeyword)
-                if (exportModifier) {
-                    addExport(fd.name)
-                    removeExport(exportModifier.pos, exportModifier.end);
-                }
-            }
-            addDeclaredName(fd.name);
-        }
-
-        if (s.kind == SyntaxKind.ExportDeclaration) {
-            let ed = s as ExportDeclaration;
-            for (let ne of ed.exportClause.elements) {
-                if (ne.propertyName) {
-                    addExport(ne.propertyName, ne.name)
-                } else {
-                    addExport(ne.name)
-                }
-                //we can remove entire modifier
-                removeExport(ed.pos, ed.end);
-            }
-        }
-    }
-
-    return { exportedNames, declaredNames }
-}
-
-function processModuleScriptTag(str: MagicString, script: Node) {
-    let htmlx = str.original;
-
-    let scriptStartTagEnd = htmlx.indexOf(">", script.start)+1;
-    let scriptEndTagStart = htmlx.lastIndexOf("<", script.end-1);
-  
-    str.overwrite(script.start, scriptStartTagEnd, "</>;");
-    str.overwrite(scriptEndTagStart, script.end, ";<>");
-}
-
-
-type InstanceScriptProcessResult = {
-    exportedNames: Map<string, string>;
-    uses$$props: boolean;
-}
-
-function processInstanceScriptContent(str: MagicString, script: Node): InstanceScriptProcessResult {
-    let htmlx = str.original;
-    let tsAst = createSourceFile("component.ts.svelte", htmlx.substring(script.content.start, script.content.end), ScriptTarget.Latest, true, ScriptKind.TS);
-
-    let { exportedNames, declaredNames } = replaceExports(str, tsAst, script.content.start);
-    declareImplictReactiveVariables(declaredNames, str, tsAst, script.content.start);
-    processImports(str, tsAst, script.content.start, script.start+1);
-    
-
-    return { 
-        exportedNames,
-        uses$$props: false
-    }
-}
-
-
-function addComponentExport(str: MagicString, uses$$props: boolean) {
-    str.append(`\n\nexport default class {\n    $$prop_def = __sveltets_partial${ uses$$props ? "_with_any" : "" }(render().props)\n    $$slot_def = render().slots\n}`);
 }
 
 
@@ -250,19 +105,215 @@ function processSvelteTemplate(str: MagicString): TemplateProcessResult {
 }
 
 
-function createRenderFunction(str: MagicString, scriptTag: Node, scriptDestination: number, slots: Map<string, Map<string,string>>, exportedNames: Map<string,string>) {
+
+function declareImplictReactiveVariables(declaredNames: string[], str: MagicString, tsAst: ts.SourceFile, astOffset: number) {
+    for (let le of tsAst.statements) {
+        if (le.kind != ts.SyntaxKind.LabeledStatement) continue;
+        let ls = le as ts.LabeledStatement;
+        if (ls.label.text != "$") continue;
+        if (!ls.statement || ls.statement.kind != ts.SyntaxKind.ExpressionStatement) continue;
+        let es = ls.statement as ts.ExpressionStatement;
+        if (!es.expression || es.expression.kind != ts.SyntaxKind.BinaryExpression) continue;
+        let be = es.expression as ts.BinaryExpression;
+        if (be.operatorToken.kind != ts.SyntaxKind.EqualsToken
+            || be.left.kind != ts.SyntaxKind.Identifier) continue;
+
+        let ident = be.left as ts.Identifier;
+        //are we already declared?
+        if (declaredNames.find(n => ident.text == n)) continue;
+        //add a declaration
+        str.prependRight(ls.pos + astOffset + 1, `;let ${ident.text}; `)
+    }
+}
+
+
+
+type InstanceScriptProcessResult = {
+    exportedNames: Map<string, string>;
+    uses$$props: boolean;
+}
+
+function processInstanceScriptContent(str: MagicString, script: Node): InstanceScriptProcessResult {
     let htmlx = str.original;
+    let tsAst = ts.createSourceFile("component.ts.svelte", htmlx.substring(script.content.start, script.content.end), ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
+    let astOffset = script.content.start;
+
+    let exportedNames = new Map<string,string>();
+    let declaredTopLevelNames: Set<string> = new Set();
+    let implicitTopLevelNames: Map<string, number> = new Map();
+    let uses$$props = false;
+
+    const addDeclaredName = (name: ts.BindingName) => {
+        if (name.kind == ts.SyntaxKind.Identifier) {
+            declaredTopLevelNames.add(name.text);
+        }
+    }
+
+    const addExport = (name: ts.BindingName, target: ts.BindingName = null) => {
+        if (name.kind != ts.SyntaxKind.Identifier) {
+            throw Error("export source kind not supported " + name)
+        }
+        if (target && target.kind != ts.SyntaxKind.Identifier) {
+            throw Error("export target kind not supported " + target)
+        }
+        exportedNames.set(name.text, target ? (target as ts.Identifier).text : null);
+    }
+
+    const removeExport = (start: number, end: number) => {
+        let exportStart = str.original.indexOf("export", start+astOffset);
+        let exportEnd = exportStart + "export".length;
+        str.remove(exportStart, exportEnd);
+    }
+
+    const handleIdentifier = (ident: ts.Identifier, parent: ts.Node) => {
+        if (ident.text == "$$props") {
+            uses$$props = true;
+            return
+        }
+
+        //convert store references
+        if (!ident.text.startsWith('$')) return;
+
+        //we are a store variable
+
+        //we are on the left
+     //   if (parent && ts.isBinaryExpression(parent) && parent.operatorToken.kind == ts.SyntaxKind.EqualsToken && parent.left == ident) {
+
+      //  }
+
+
+
+    }
+
+    const processChild = (s: ts.Node, parent: ts.Node) => {
+        if (ts.isIdentifier(s)) handleIdentifier(s, parent);
+        ts.forEachChild(s, n => processChild(n, s));
+    }
+
+
+    const processTopLevelStatement = (s: ts.Statement) => {
+        if (ts.isVariableStatement(s)) {
+            let exportModifier = s.modifiers ? s.modifiers.find(x => x.kind == ts.SyntaxKind.ExportKeyword): null;
+            if (exportModifier) {
+                removeExport(exportModifier.pos, exportModifier.end);
+            }
+            for (let v of s.declarationList.declarations) {
+                if (exportModifier) {
+                    addExport(v.name);
+                }
+                addDeclaredName(v.name);
+            }
+        }
+
+        if (ts.isFunctionDeclaration(s)) {
+            if (s.modifiers) {
+                let exportModifier = s.modifiers.find(x => x.kind == ts.SyntaxKind.ExportKeyword)
+                if (exportModifier) {
+                    addExport(s.name)
+                    removeExport(exportModifier.pos, exportModifier.end);
+                }
+            }
+            addDeclaredName(s.name);
+        }
+
+        if (ts.isExportDeclaration(s)) {
+            for (let ne of s.exportClause.elements) {
+                if (ne.propertyName) {
+                    addExport(ne.propertyName, ne.name)
+                } else {
+                    addExport(ne.name)
+                }
+                //we can remove entire statement
+                removeExport(s.pos, s.end);
+            }
+        }
+
+        //move imports to top of script so they appear outside our render function
+        if (ts.isImportDeclaration(s)) {
+            str.move(s.pos+astOffset, s.end+astOffset, script.start+1);
+            str.overwrite(s.end+astOffset-1, s.end+astOffset, '"\n')
+
+            //track the top level declaration
+            if (s.importClause) {
+                if (s.importClause.name && ts.isIdentifier(s.importClause.name)) {
+                    addDeclaredName(s.importClause.name)
+                } 
+                if (s.importClause.namedBindings && ts.isNamedImports(s.importClause.namedBindings)) {
+                    for(let i of s.importClause.namedBindings.elements) {
+                        if (ts.isIdentifier(i.name)) {
+                            addDeclaredName(i.name)
+                        }
+                    }
+                }
+            }
+        }
+
+        //track implicit declarations in reactive blocks
+        if (ts.isLabeledStatement(s) 
+                && s.label.text == "$"
+                && s.statement 
+                && ts.isExpressionStatement(s.statement)
+                && ts.isBinaryExpression(s.statement.expression)
+                && s.statement.expression.operatorToken.kind == ts.SyntaxKind.EqualsToken
+                && ts.isIdentifier(s.statement.expression.left))   {
+                    
+            implicitTopLevelNames.set(s.statement.expression.left.text, s.label.pos );
+        }
+        
+        //handle stores etc
+        if (ts.isIdentifier(s)) handleIdentifier(s, null);
+        
+        //to save a bunch of condition checks on each node, we recurse into processChild which skips all the checks for top level items
+        return ts.forEachChild(s, n => processChild(n, s))
+    }
+
+    tsAst.forEachChild(processTopLevelStatement);
+
+    // declare implicit reactive variables
+    for ( var [name, pos]  of implicitTopLevelNames.entries()) {
+        if (!declaredTopLevelNames.has(name)) {
+            //add a declaration
+            str.prependRight(pos + astOffset + 1, `;let ${name}; `)
+        }
+    }
+
+    return { 
+        exportedNames,
+        uses$$props
+    }
+}
+
+
+function addComponentExport(str: MagicString, uses$$props: boolean) {
+    str.append(`\n\nexport default class {\n    $$prop_def = __sveltets_partial${ uses$$props ? "_with_any" : "" }(render().props)\n    $$slot_def = render().slots\n}`);
+}
+
+function processModuleScriptTag(str: MagicString, script: Node) {
+    let htmlx = str.original;
+
+    let scriptStartTagEnd = htmlx.indexOf(">", script.start)+1;
+    let scriptEndTagStart = htmlx.lastIndexOf("<", script.end-1);
+  
+    str.overwrite(script.start, scriptStartTagEnd, "</>;");
+    str.overwrite(scriptEndTagStart, script.end, ";<>");
+}
+
+
+
+function createRenderFunction(str: MagicString, scriptTag: Node, scriptDestination: number, slots: Map<string, Map<string,string>>, exportedNames: Map<string,string>, uses$$props: boolean) {
+    let htmlx = str.original;
+    let propsDecl =  uses$$props ? " let $$props: SvelteAllProps;" : ""
 
     if (scriptTag) {
         //I couldn't get magicstring to let me put the script before the <> we prepend during conversion of the template to jsx, so we just close it instead
         let scriptTagEnd = htmlx.lastIndexOf(">", scriptTag.content.start) + 1;
         str.overwrite(scriptTag.start, scriptTag.start+ 1, "</>;");
-        str.overwrite(scriptTag.start+1, scriptTagEnd, "function render() {\n");
+        str.overwrite(scriptTag.start+1, scriptTagEnd, `function render() {${propsDecl}\n`);
 
         let scriptEndTagStart = htmlx.lastIndexOf("<", scriptTag.end-1);
         str.overwrite(scriptEndTagStart, scriptTag.end, ";\n<>");
     } else {
-        str.prependRight(scriptDestination, "</>;function render() {\n<>");
+        str.prependRight(scriptDestination, `</>;function render() {${propsDecl}\n<>`);
     }
 
     let returnElements = [...exportedNames.entries()].map(([key, value]) => value ? `${value}: ${key}` : key);
@@ -313,7 +364,7 @@ export function svelte2tsx(svelte: string) {
     }
     
     //wrap the script tag and template content in a function returning the slot and exports
-    createRenderFunction(str, scriptTag, instanceScriptTarget, slots, exportedNames);
+    createRenderFunction(str, scriptTag, instanceScriptTarget, slots, exportedNames, uses$$props);
     
     // we need to process the module script after the instance script has moved otherwise we get warnings about moving edited items
     if (moduleScriptTag) {
