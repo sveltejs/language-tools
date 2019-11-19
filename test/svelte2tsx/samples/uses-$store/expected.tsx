8f17499af523677e868b81aba311fe3de4c60073
David Pershouse <dpershouse@gmail.com>
2019-11-19 18:13:25 +1000

    store transformation complete

--- test/svelte2tsx/samples/uses-$store/expected.tsx
+++ test/svelte2tsx/samples/uses-$store/expected.tsx
@@ -1,7 +1,7 @@
 <></>;function render() {
 b.set(__sveltets_store_get(b).concat(5));
 <>
-<h1>{__sveltets_store_get(b)}</h1></>
+<h1 onclick={() => b.set(__sveltets_store_get(b).concat(5))}>{__sveltets_store_get(b)}</h1></>
 return { props: {}, slots: {} }}
 
 export default class {
