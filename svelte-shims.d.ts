36ca01f4147d52045fc99d1014c98ba53b30dd3c
David Pershouse <dpershouse@gmail.com>
2019-11-29 10:10:28 +1000

    smarter shims

--- svelte-shims.d.ts
+++ svelte-shims.d.ts
@@ -32,15 +32,15 @@ type SvelteAllProps = {	[index: string]: any }
 
 type SvelteStore<T> =  { subscribe: (value:T) => any }
 
-declare function __sveltets_ensureAnimation<U extends any[]>(animation: SvelteAnimation<U>, ...args: U): any[];
+declare function __sveltets_ensureAnimation<U extends any[]>(animation: SvelteAnimation<U>, ...args: U): any;
 
-declare function __sveltets_ensureAction<U extends any[]>(action: SvelteAction<U>, ...args: U): any[];
+declare function __sveltets_ensureAction<U extends any[]>(action: SvelteAction<U>, ...args: U): any;
 
-declare function __sveltets_ensureTransition<U extends any[]>(transition: SvelteTransition<U>, ...args: U): any[];
+declare function __sveltets_ensureTransition<U extends any[]>(transition: SvelteTransition<U>, ...args: U): any;
 
-declare function __sveltets_ensureFunction(expression: (e: Event) => unknown ):any[];
+declare function __sveltets_ensureFunction(expression: (e: Event) => unknown ):any;
 
-declare function __sveltets_ensureType<T>(type: AConstructorTypeOf<T>, el: T): any[];
+declare function __sveltets_ensureType<T>(type: AConstructorTypeOf<T>, el: T): any;
 declare function __sveltets_instanceOf<T>(type: AConstructorTypeOf<T>): T;
 
 declare function __sveltets_partial<T>(obj: T): Partial<T>;
