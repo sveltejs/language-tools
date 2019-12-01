2177709ccf5129ff000fda610ab99b07f382d4cb
halfnelson <dpershouse@gmail.com>
2019-12-01 17:34:51 +1000

    failing test for one way bindings

---
+++ test/htmlx2jsx/samples/binding-oneway/expected.jsx
@@ -0,0 +1,16 @@
+<><div type="text" 
+    {...__sveltets_any(width=__sveltets_block_element.clientWidth)} 
+    clientHeight={height} 
+    offsetWidth={offsetWidth} 
+    offsetHeight={offsetHeight}
+/>
+
+<video
+ src={clip}
+ duration={duration}
+ buffered={buffered}
+ seekable={seekable}
+ seeking={seeking}
+ played={played}
+ ended={ended}
+></video></>
