adfb8ef5bb6a8b4e13d397df0a8b1dc71f7a8416
halfnelson <dpershouse@gmail.com>
2019-12-06 22:52:41 +1000

    couple of things to test source_map

--- test/sourcemaps.solo/index.js
+++ test/sourcemaps.solo/index.js
@@ -6,10 +6,16 @@ let sm = require('source-map')
 
 
 describe('htmlx2jsx', () => {
-
+	/**
+	 * 
+	 * @param {string} input 
+	 * 
+	 * @returns { {source: string, locations: Map<String,{ line: number, start: number, offsets:number[] }}> }
+	 */
 	function extractLocations(input) {
 		let lines = input.split("\n")
 		let line
+		let source_line = 0;
 		let source = []
 		let locations = new Map()
 		while (line = lines.shift()) {
@@ -17,22 +23,37 @@ describe('htmlx2jsx', () => {
 			if (/^\s*\d/.test(line)) {
 				//create the ranges
 				let currentId = null
-				let idOffset = 0
+				let offset = 0
+				let offsets = []
+				let start = 0
+				const endSpan = () => {
+					if (offsets.length) {
+						locations.set(currentId, { line: source_line, start: start, offsets:offsets });
+					}
+					offset = 0
+					offsets = []
+				}
+
 				for (let char = 0; char < line.length; char++) {
 					let c = line[char]
 					let isDigit = /\d/.test(c), isEquals = /=/.test(c)
 					if (isDigit) {
+						endSpan()
 						currentId = c
-						idOffset = 0
+						start = char + 1;
 					}
 					if (isEquals || isDigit) {
-						locations.set(`${source.length}:${char + 1}`, currentId+`-${idOffset}`)
-						idOffset++;
+						offsets.push(offset)
+						offset++;
+					} else {
+						endSpan()
 					}
 				}
+				endSpan();
 			} else {
 				//we are a source line
 				source.push(line)
+				source_line++;
 			}
 		}
 
@@ -58,41 +79,80 @@ describe('htmlx2jsx', () => {
 			const testContent = fs.readFileSync(`${__dirname}/${dir}`, 'utf-8').replace(/\s+$/, '').replace(/\r\n/g, "\n");
 
 			let [inputBlock, expectedBlock] = testContent.split(/\n!Expected.*?\n/)
-
-		
-
-			let original = extractLocations(inputBlock);
+			let input = extractLocations(inputBlock);
 			let expected = extractLocations(expectedBlock);
-			const getFrame = (source,line,col) => {
-				return `${source.split("\n")[line-1]}\n${col > 1 ? new Array(col-1).fill(" ").join("") : ""}^`	
-			}
-			/**
-			 * Retrieves the location in the original source for the given mapped id
-			 * @param {Map<String, String>} locations 
-			 * @param {String} id 
-			 */
-			const locForId = (locations, id) => {
-				let match = [...locations.entries()].find( ([loc,oid]) => id == oid);
-				if (!match) throw new Error("Id lookup failed! "+id)
-				let [loc, _] = match
-				return loc;
-			}
-
-			const { map, code } = converter.htmlx2jsx(original.source);
-			assert.equal(code, expected.source);
+			
+		
+			// seems backwards but we don't have an "input" source map, so we generate one from our expected output
+			// but assert that the source it generates matches our input source.
+			const { map, code } = converter.htmlx2jsx(expected.source);
+			assert.equal(code, input.source, "Couldn't generate input source map for test");
 
 			let decoder = new sm.SourceMapConsumer(map);
-			for (let [loc, id] of expected.locations.entries()) {
-				let [ lineStr, colStr] = loc.split(":");
-				let line = Number(lineStr), col = Number(colStr)
-				let o = decoder.originalPositionFor({line: line, column: col});
-			    let eo = locForId(original.locations,id)
-				let [ eline, ecol] = eo.split(":")
-				console.log(`${id} ${o.line}:${o.column}->${eline}:${ecol}`)
-				assert.equal(`${o.line}:${o.column}`,eo, `\nid:${id}\nactual:\n${getFrame(original.source, o.line, o.column)}\nexpected:\n${getFrame(original.source, eline,ecol)}`);
-				//console.log(`${id} ${o.line}:${o.column}=${eline}:${ecol}`)
-			}
+			
+			for (let [id, span] of input.locations.entries()) {
+				let expectedSpan = expected.locations.get(id);
+	
+				//walk our generated span checking it lines up
+				let col = span.start
+				let input_line = span.line
+				let expected_line = expectedSpan.line
 
+				
+				assert.ok(input.source);
+				let error_source = input.source.split("\n")[span.line-1];
+				assert.ok(error_source);
+				let error_map = new Array(error_source.length).fill(" ");
+			
+				let actual_result_line, actual_result_source, actual_result
+				let errorCount = 0;
+				
+				for (var off = 0; off < span.offsets.length; off++) {
+					let input_col = col + off;
+					let expected_col = expectedSpan.start + span.offsets[off];
+					
+					//originalPositionFor uses 0 base cols and 1 base lines....
+					let { line: actual_line, column: decoded_col } = decoder.originalPositionFor({ line: input_line, column: input_col - 1 })
+					let actual_col = decoded_col + 1;
+					
+					if (!actual_result) {
+						actual_result_source = expected.source.split("\n")[actual_line-1]
+						 actual_result = new Array(actual_result_source.length).fill(" ");
+						 actual_result_line = actual_line
+					}
+					if (actual_line == actual_result_line) {
+						if (actual_result_line[actual_col - 1] == " ") {
+							actual_result[actual_col - 1] = "1"
+						} else {
+							//track number of characters mapped to result
+							actual_result[actual_col - 1] = `${Math.min((actual_result[actual_col - 1] << 0) + 1, 9)}`
+						}
+					} else {
+						actual_result = actual_result + "X"
+					}
+
+					if (actual_col != expected_col || actual_line != expected_line) {
+						errorCount++;
+						error_map[input_col-1] = "X"
+					} else {
+						error_map[input_col-1] = "="
+					}
+				}
+
+				if (errorCount != 0) {
+					assert.fail(`
+					Errors on span ${id}
+			
+					Output
+					${actual_result_source}
+					${actual_result.join("").replace(/1/g,"=")}
+
+					Errors
+					${error_source}
+					${error_map.join("")}
+					`)
+				} 
+			}
 		});
 	});
 });
