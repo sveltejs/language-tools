a0bf105272e8fab00fa07213053005f4ba2a5725
Steve Le Roy Harris <steve.leroyharris@seedrs.com>
2020-02-15 16:41:06 +0000

    Add more tests for exported props

---
+++ test/svelte2tsx/samples/export-arrow-function/input.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let f = (a: number, b: number) => {
+      let c = a + b;
+      return c;
+    }
+</script>
---
+++ test/svelte2tsx/samples/export-has-type/expected.tsx
@@ -0,0 +1,12 @@
+<></>;function render() {
+
+    interface A {}
+     let a: A;
+;
+<></>
+return { props: {a}, slots: {} }}
+
+export default class {
+    $$prop_def = __sveltets_partial(render().props)
+    $$slot_def = render().slots
+}
