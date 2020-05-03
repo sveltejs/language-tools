07b9a5894a8ce24a85a8164b5cbe52ad0d5185b0
Sibtain Jafferi <msajaffe@uwaterloo.ca>
2020-05-03 14:48:14 -0700

    Added test cases to ensure correct attribute extraction

--- packages/language-server/test/lib/documents/utils.test.ts
+++ packages/language-server/test/lib/documents/utils.test.ts
@@ -17,6 +17,16 @@ describe('document/utils', () => {
                 container: { start: 101, end: 133 },
             });
         });
+        it('does not extract tags starting with style/script', () => {
+            // https://github.com/sveltejs/language-tools/issues/43
+            // this would previously match <styles>....</style> due to misconfigured attribute matching regex
+            const text = `
+            <styles>p{ color: blue; }</styles>
+            <p>bla</p>
+            ></style>
+            `;
+            assert.deepStrictEqual(extractTag(text, 'style'), null);
+        });
         it('extracts style tag', () => {
             const text = `
                 <p>bla</p>
@@ -30,15 +40,29 @@ describe('document/utils', () => {
                 container: { start: 44, end: 76 },
             });
         });
-        it('does not extract tags starting with style/script', () => {
-            // https://github.com/sveltejs/language-tools/issues/43
-            // this would previously match <styles>....</style> due to misconfigured attribute matching regex
+        it('extracts style tag with attributes', () => {
             const text = `
-            <styles>p{ color: blue; }</styles>
-            <p>bla</p>
-            ></style>
+                <style lang="scss">p{ color: blue; }</style>
             `;
-            assert.deepStrictEqual(extractTag(text, 'style'), null);
+            assert.deepStrictEqual(extractTag(text, 'style'), {
+                content: 'p{ color: blue; }',
+                attributes: { lang: 'scss' },
+                start: 36,
+                end: 53,
+                container: { start: 17, end: 61 },
+            });
+        });
+        it('extracts style tag with attributes and extra whitespace', () => {
+            const text = `
+                <style     lang="scss"    >  p{ color: blue; }  </style>
+            `;
+            assert.deepStrictEqual(extractTag(text, 'style'), {
+                content: '  p{ color: blue; }  ',
+                attributes: { lang: 'scss' },
+                start: 44,
+                end: 65,
+                container: { start: 17, end: 73 },
+            });
         });
     });
 });
