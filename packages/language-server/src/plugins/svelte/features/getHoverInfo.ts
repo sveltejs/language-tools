3bd7b3385086884adb4679a4c6a6c556904e3bbd
Simon H <5968653+dummdidumm@users.noreply.github.com>
2020-05-11 14:38:42 +0200

    (feat) hover info for svelte tags (#66)
    * (feat) hover info for svelte tags
    
    hover info for tags such a #if, :else, :then, ...
    
    * (feat) add documentation links to svelte tags

---
+++ packages/language-server/src/plugins/svelte/features/getHoverInfo.ts
@@ -0,0 +1,88 @@
+import { Hover, Position } from 'vscode-languageserver';
+import { SvelteDocument } from '../SvelteDocument';
+import { documentation, SvelteTag, getLatestOpeningTag } from './SvelteTags';
+import { flatten } from '../../../utils';
+
+/**
+ * Get hover information for special svelte tags within moustache tags.
+ */
+export function getHoverInfo(svelteDoc: SvelteDocument, position: Position): Hover | null {
+    const offset = svelteDoc.offsetAt(position);
+
+    const isInStyleOrScript =
+        svelteDoc.style.isInFragment(position) || svelteDoc.script.isInFragment(position);
+
+    const offsetStart = Math.max(offset - 10, 0);
+    const charactersAroundOffset = svelteDoc
+        .getText()
+        // use last 10 and next 10 characters, should cover 99% of all cases
+        .substr(offsetStart, 20);
+    const isNoSvelteTag = !tagRegexp.test(charactersAroundOffset);
+
+    if (isInStyleOrScript || isNoSvelteTag) {
+        return null;
+    }
+
+    const tag = getTagAtOffset(svelteDoc, offsetStart, charactersAroundOffset, offset);
+    if (!tag) {
+        return null;
+    }
+
+    return { contents: documentation[tag] };
+}
+
+/**
+ * Get the tag that is at the offset.
+ */
+function getTagAtOffset(
+    svelteDoc: SvelteDocument,
+    charactersOffset: number,
+    charactersAroundOffset: string,
+    offset: number,
+): SvelteTag | null {
+    const foundTag = tagPossibilities.find((tagAndValues) =>
+        tagAndValues.values.find((value) =>
+            isAroundOffset(charactersOffset, charactersAroundOffset, value, offset),
+        ),
+    );
+
+    if (!foundTag) {
+        return null;
+    }
+    if (foundTag.tag !== ':else') {
+        return foundTag.tag;
+    }
+    // ':else can be part of one of each, await, if --> find out which one
+    return getLatestOpeningTag(svelteDoc, offset);
+}
+
+function isAroundOffset(
+    charactersOffset: number,
+    charactersAroundOffset: string,
+    toFind: string,
+    offset: number,
+) {
+    const match = charactersAroundOffset.match(toFind);
+    if (!match || match.index === undefined) {
+        return false;
+    }
+    const idx = match.index + charactersOffset;
+    return idx <= offset && idx + toFind.length >= offset;
+}
+
+const tagPossibilities: { tag: SvelteTag | ':else'; values: string[] }[] = [
+    { tag: 'if' as const, values: ['#if', '/if', ':else if'] },
+    // each
+    { tag: 'each' as const, values: ['#each', '/each'] },
+    // await
+    { tag: 'await' as const, values: ['#await', '/await', ':then', ':catch'] },
+    // @
+    { tag: 'html' as const, values: ['@html'] },
+    { tag: 'debug' as const, values: ['@debug'] },
+    // this tag has multiple possibilities
+    { tag: ':else' as const, values: [':else'] },
+];
+
+const tagRegexp = new RegExp(
+    `[\\s\\S]*{\\s*(${flatten(tagPossibilities.map((p) => p.values)).join('|')})(\\s|})`,
+);
