3f7e9c432c545d2db15bb1eaf5426a5ace600aad
Lyu, Wei-Da <36730922+jasonlyu123@users.noreply.github.com>
2020-05-28 16:14:02 +0800

    fix destructuring in slot props (let:name={{val}}) (#123)

---
+++ packages/svelte2tsx/test/htmlx2jsx/samples/component-default-slot-let-destructure/expected.jsx
@@ -0,0 +1,3 @@
+<><Component >{() => { let {thing:{ a }} = __sveltets_instanceOf(Component).$$slot_def.default;<>
+    <h1>Hello { a }</h1>
+</>}}</Component></>
---
+++ packages/svelte2tsx/test/htmlx2jsx/samples/component-default-slot-let-destructure/input.svelte
@@ -0,0 +1,3 @@
+<Component let:thing={{ a }}>
+    <h1>Hello { a }</h1>
+</Component>
