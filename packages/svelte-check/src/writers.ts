5087bd734f5ee82674338044283fe720ba735517
Ron Don Jon <ron.don.jon>
2020-05-28 17:12:42 +0200

    Refactor console output & add machine-readable output option

---
+++ packages/svelte-check/src/writers.ts
@@ -0,0 +1,124 @@
+import * as chalk from 'chalk';
+import { Writable } from "stream";
+import {
+    Diagnostic,
+    DiagnosticSeverity,
+} from 'vscode-languageserver-protocol';
+import { offsetAt } from 'svelte-language-server';
+
+export interface Writer {
+    start: (workspaceUri: string) => void;
+    file: (d: Diagnostic[], filename: string, text: string) => void;
+    completion: (fileCount: number, errorCount: number, warningCount: number) => void;
+    failure: (err: Error) => void;
+}
+
+export class HumanFriendlyWriter implements Writer {
+    _stream: Writable;
+    _isVerbose: boolean;
+
+    constructor(stream: Writable, isVerbose = true) {
+        this._stream = stream;
+        this._isVerbose = isVerbose;
+    }
+
+    start(workspaceUri: string) {
+        if (this._isVerbose) {
+            this._stream.write('\n');
+            this._stream.write(`Loading svelte-check in workspace: ${workspaceUri}`);
+            this._stream.write('\n');
+            this._stream.write('Getting Svelte diagnostics...\n');
+            this._stream.write('====================================\n');
+            this._stream.write('\n');
+        }
+    }
+
+    file(diagnostics: Diagnostic[], filename: string, text: string): void {
+        if (diagnostics.length > 0) {
+            this._stream.write('\n');
+            this._stream.write(`${chalk.green('File')} : ${chalk.green(filename)}\n`);
+
+            diagnostics.forEach((diagnostic) => {
+                const source = diagnostic.source ? `(${diagnostic.source})` : '';
+
+                // eslint-disable-next-line max-len
+                const position = `Line: ${diagnostic.range.start.line}, Character: ${diagnostic.range.start.character}`;
+
+                // Show some context around diagnostic range
+                const startOffset = offsetAt(diagnostic.range.start, text);
+                const endOffset = offsetAt(diagnostic.range.end, text);
+                const codePrev = chalk.cyan(text.substring(Math.max(startOffset - 10, 0), startOffset));
+                const codeHighlight = chalk.magenta(text.substring(startOffset, endOffset));
+                const codePost = chalk.cyan(text.substring(endOffset, endOffset + 10));
+                const code = codePrev + codeHighlight + codePost;
+                let msg;
+
+                if (this._isVerbose) {
+                    msg = `${diagnostic.message} ${source}\n${position}\n${chalk.cyan(code)}`;
+                }
+                else {
+                    msg = diagnostic.message;
+                }
+
+                if (diagnostic.severity === DiagnosticSeverity.Error) {
+                    this._stream.write(`${chalk.red('Error')}: ${msg}\n`);
+                }
+                else {
+                    this._stream.write(`${chalk.yellow('Warn')}: ${msg}\n`);
+                }
+            });
+
+            this._stream.write('\n');
+        }
+    }
+
+    completion(_fileCount: number, errorCount: number, _warningCount: number) {
+        this._stream.write('====================================\n');
+
+        if (errorCount === 0) {
+            this._stream.write(chalk.green(`svelte-check found no errors\n`));
+        } else {
+            this._stream.write(chalk.red(`svelte-check found ${errorCount} ${errorCount === 1 ? 'error' : 'errors'}\n`));
+        }
+    }
+
+    failure(err: Error) {
+        this._stream.write(`${err}\n`);
+    }
+}
+
+export class MachineFriendlyWriter implements Writer {
+    _stream: Writable;
+
+    constructor(stream: Writable) {
+        this._stream = stream;
+    }
+
+    _log(msg: string) {
+        this._stream.write(`${new Date().getTime()} ${msg}\n`);
+    }
+
+    start(workspaceUri: string) {
+        this._log(`START ${JSON.stringify(workspaceUri)}`);
+    }
+
+    file(diagnostics: Diagnostic[], filename: string, _text: string) {
+        diagnostics.forEach((d) => {
+            const { message, severity, range } = d;
+            const type = severity === DiagnosticSeverity.Error ? "ERROR" : severity === DiagnosticSeverity.Warning ? "WARNING" : null;
+
+            if (type) {
+                const { line, character } = range.start;
+                this._log(`${type} ${JSON.stringify(filename)} ${line}:${character} ${JSON.stringify(message)}`);
+            }
+        });
+    }
+
+    completion(fileCount: number, errorCount: number, warningCount: number) {
+        this._log(`COMPLETED ${fileCount} FILES ${errorCount} ERRORS ${warningCount} WARNINGS`);
+    }
+
+    failure(err: Error) {
+        this._log(`FAILURE ${JSON.stringify(err.message)}`);
+    }
+}
