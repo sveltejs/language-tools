2aca27d923a0719af9f53623a509dd2467394eeb
Simon H <5968653+dummdidumm@users.noreply.github.com>
2020-06-23 14:26:48 +0200

    (fix) fine-grained control over incomplete completions (#224)
    - html/css: only set to incomplete if there are emmet results
    - no more white space trigger, causes too many problems/unnecessary completion triggers
    - filter all completions in the language server if not set otherwise. VSCode will filter client side so it's turned off for it.
    
    Fixes #217

--- packages/language-server/test/utils.test.ts
+++ packages/language-server/test/utils.test.ts
@@ -1,4 +1,4 @@
-import { isBeforeOrEqualToPosition } from '../src/utils';
+import { isBeforeOrEqualToPosition, regexLastIndexOf } from '../src/utils';
 import { Position } from 'vscode-languageserver';
 import * as assert from 'assert';
 
@@ -27,6 +27,20 @@ describe('utils', () => {
         it('is after position (line lower, character higher)', () => {
             const result = isBeforeOrEqualToPosition(Position.create(1, 1), Position.create(2, 0));
             assert.equal(result, false);
+        });
+    });
+
+    describe('#regexLastIndexOf', () => {
+        it('should work #1', () => {
+            assert.equal(regexLastIndexOf('1 2 3', /\s/g), 3);
+        });
+
+        it('should work #2', () => {
+            assert.equal(regexLastIndexOf('1_2:- 3', /\W/g), 5);
+        });
+
+        it('should work #3', () => {
+            assert.equal(regexLastIndexOf('<bla blubb={() => hello', /[\W\s]/g), 17);
         });
     });
 });
