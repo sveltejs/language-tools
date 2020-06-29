d7759fe0300b9cf90f7c71025372a16ca21529aa
Christian Kaisermann <christian@kaisermann.me>
2020-06-29 09:37:35 -0300

    (fix) svelte-preprocess name typo (#246)

--- packages/language-server/test/plugins/svelte/features/getDiagnostics.test.ts
+++ packages/language-server/test/plugins/svelte/features/getDiagnostics.test.ts
@@ -197,7 +197,7 @@ describe('SveltePlugin#getDiagnostics', () => {
                 message:
                     'expected x to not be here' +
                     '\n\nIf you expect this syntax to work, here are some suggestions: ' +
-                    '\nIf you use typescript with `svelte-preprocessor`, did you add `lang="typescript"` to your `script` tag? ' +
+                    '\nIf you use typescript with `svelte-preprocess`, did you add `lang="typescript"` to your `script` tag? ' +
                     '\nDid you setup a `svelte.config.js`? ' +
                     '\nSee https://github.com/sveltejs/language-tools/tree/master/packages/svelte-vscode#using-with-preprocessors for more info.',
                 range: {
