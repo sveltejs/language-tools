609315b5ee107586174399586433417f27e5a69e
Simon H <5968653+dummdidumm@users.noreply.github.com>
2020-06-30 20:07:27 +0200

    (fix) remove false positive warning (#252)
    !$store does not need special treatment
    
    #245

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/uses-$store-with-exclamation-mark/expected.tsx
@@ -0,0 +1,16 @@
+<></>;import { writable } from 'svelte/store';
+function render() {
+
+  
+  const count = writable(0);
+  const handler1 = () => !__sveltets_store_get(count)
+;
+<>
+
+<button onclick={() => !__sveltets_store_get(count)}>add</button></>
+return { props: {}, slots: {} }}
+
+export default class {
+    $$prop_def = __sveltets_partial(render().props)
+    $$slot_def = render().slots
+}
