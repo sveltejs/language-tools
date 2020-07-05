606243b159cc28119bce55e5d0655cf133c26326
Patrick <Patrick@ShowYou.us>
2020-07-05 20:09:41 +0200

    Determinate if prop is optional based on whether the variable has an initializer

--- packages/svelte2tsx/test/svelte2tsx/samples/export-has-type/input.svelte
+++ packages/svelte2tsx/test/svelte2tsx/samples/export-has-type/input.svelte
@@ -1,4 +1,5 @@
 <script>
     interface A {}
     export let a: A;
+    export let b: A = {};
 </script>
--- packages/svelte2tsx/test/svelte2tsx/samples/typed-export-with-default/expected.tsx
+++ packages/svelte2tsx/test/svelte2tsx/samples/typed-export-with-default/expected.tsx
@@ -3,7 +3,7 @@
      let name: string | number = "world";name = __sveltets_any(name);
 ;
 <></>
-return { props: {name: name} as {name: string | number}, slots: {} }}
+return { props: {name: name} as {name?: string | number}, slots: {} }}
 
 export default class {
     $$prop_def = __sveltets_partial(render().props)
