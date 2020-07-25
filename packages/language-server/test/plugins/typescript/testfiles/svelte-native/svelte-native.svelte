d15939cfe168868ebaf188342b914b29588709c0
halfnelson <dpershouse@gmail.com>
2020-07-26 02:07:09 +1000

    Toggle JSX namespace based on svelte vs svelte-native (#351)
    * Toggle JSX namespace based on svelte vs svelte-native
    
    * add svelte-native-jsx.d.ts to the package
    
    * lint fixes

---
+++ packages/language-server/test/plugins/typescript/testfiles/svelte-native/svelte-native.svelte
@@ -0,0 +1,12 @@
+<page>
+    <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
+        <formattedString>
+            <span class="fas" text="&#xf135;" />
+            <span text=" {message}" />
+        </formattedString>
+    </label>    
+</page>
+
+<script lang="typescript">
+    let message: string = "Blank Svelte Native App"
+</script>
