8947ac3e1c0363748c148a992c6d43e4d72601c7
Lyu Jason <b00605009@ntu.edu.tw>
2020-07-26 13:15:47 +0800

    fix bind:this used before assign

--- packages/svelte2tsx/src/htmlxtojsx.ts
+++ packages/svelte2tsx/src/htmlxtojsx.ts
@@ -204,8 +204,12 @@ export function convertHtmlxToJsx(
 
             if (thisType) {
                 str.remove(attr.start, attr.expression.start);
-                str.appendLeft(attr.expression.start, `{...__sveltets_ensureType(${thisType}, `);
-                str.overwrite(attr.expression.end, attr.end, ')}');
+                str.appendLeft(attr.expression.start, '{...__sveltets_empty(');
+                str.overwrite(
+                    attr.expression.end,
+                    attr.end,
+                    `=__sveltets_instanceOf(${thisType}))}`
+                );
                 return;
             }
         }
--- packages/svelte2tsx/test/htmlx2jsx/samples/binding-this-component/expected.jsx
+++ packages/svelte2tsx/test/htmlx2jsx/samples/binding-this-component/expected.jsx
@@ -1,1 +1,1 @@
-<><Component type="radio" {...__sveltets_ensureType(Component, element)} value="Plain"/></>
+<><Component type="radio" {...__sveltets_empty(element=__sveltets_instanceOf(Component))} value="Plain"/></>
