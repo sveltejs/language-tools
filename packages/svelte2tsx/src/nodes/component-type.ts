2d67386e37f5868f0034de029139aad79b6d61ff
Lyu Jason <jason.lyu@supergeo.com.tw>
2020-08-04 13:29:23 +0800

    fix svelte:component on:event

---
+++ packages/svelte2tsx/src/nodes/component-type.ts
@@ -0,0 +1,9 @@
+import { Node } from 'estree-walker';
+
+export function getTypeForComponent(node: Node) {
+    if (node.name === 'svelte:component' || node.name === 'svelte:self') {
+        return '__sveltets_componentType()';
+    } else {
+        return node.name;
+    }
+};
