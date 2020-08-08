5fe799c1ab669b995bc483b25c07cb033eb25ed2
Simon H <5968653+dummdidumm@users.noreply.github.com>
2020-08-08 08:16:46 +0200

    (fix) bubble events (#433)
    * (fix) bubble events
    
    Added new property $$events_def to bubble events
    Also removed some obsolete types
    
    * add union type for multiple events

--- packages/svelte2tsx/test/svelte2tsx/samples/event-bubble-component-multi/expected.tsx
+++ packages/svelte2tsx/test/svelte2tsx/samples/event-bubble-component-multi/expected.tsx
@@ -1,7 +1,7 @@
 <></>;function render() {
 <><Button ></Button>
 <Radio ></Radio></>
-return { props: {}, slots: {}, getters: {}, events: {'click':[__sveltets_bubbleEventDef(__sveltets_instanceOf(Button).$on, 'click'),__sveltets_bubbleEventDef(__sveltets_instanceOf(Radio).$on, 'click')]} }}
+return { props: {}, slots: {}, getters: {}, events: {'click':__sveltets_unionType(__sveltets_bubbleEventDef(__sveltets_instanceOf(Button).$$events_def, 'click'),__sveltets_bubbleEventDef(__sveltets_instanceOf(Radio).$$events_def, 'click'))} }}
 
 export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(render)) {
 }
--- packages/svelte2tsx/test/svelte2tsx/samples/event-bubble-component/expected.tsx
+++ packages/svelte2tsx/test/svelte2tsx/samples/event-bubble-component/expected.tsx
@@ -1,6 +1,6 @@
 <></>;function render() {
 <><Button ></Button></>
-return { props: {}, slots: {}, getters: {}, events: {'click':__sveltets_bubbleEventDef(__sveltets_instanceOf(Button).$on, 'click')} }}
+return { props: {}, slots: {}, getters: {}, events: {'click':__sveltets_bubbleEventDef(__sveltets_instanceOf(Button).$$events_def, 'click')} }}
 
 export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(render)) {
 }
