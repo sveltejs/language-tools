f930623ce1d7eecbd4ae279abfde817a94074107
Lyu Jason <b00605009@ntu.edu.tw>
2020-08-16 22:59:46 +0800

    indent

--- packages/svelte2tsx/src/nodes/TemplateScope.ts
+++ packages/svelte2tsx/src/nodes/TemplateScope.ts
@@ -5,14 +5,14 @@ import { Identifier } from '../interfaces';
  * adopted from https://github.com/sveltejs/svelte/blob/master/src/compiler/compile/nodes/shared/TemplateScope.ts
  */
 export default class TemplateScope {
-	names: Set<string>;
+    names: Set<string>;
     owners: Map<string, Node> = new Map();
     inits: Map<string, Identifier> = new Map();
-	parent?: TemplateScope;
+    parent?: TemplateScope;
 
-	constructor(parent?: TemplateScope) {
-		this.parent = parent;
-		this.names = new Set(parent ? parent.names : []);
+    constructor(parent?: TemplateScope) {
+        this.parent = parent;
+        this.names = new Set(parent ? parent.names : []);
     }
 
     addMany(inits: Identifier[], owner: Node) {
@@ -20,29 +20,29 @@ export default class TemplateScope {
         return this;
     }
 
-	add(init: Identifier, owner: Node) {
+    add(init: Identifier, owner: Node) {
         const { name } = init;
-		this.names.add(name);
+        this.names.add(name);
         this.inits.set(name, init);
-		this.owners.set(name, owner);
-		return this;
-	}
+        this.owners.set(name, owner);
+        return this;
+    }
 
-	child() {
-		const child = new TemplateScope(this);
-		return child;
-	}
+    child() {
+        const child = new TemplateScope(this);
+        return child;
+    }
 
-	getOwner(name: string): Node {
-		return this.owners.get(name) || (this.parent && this.parent.getOwner(name));
+    getOwner(name: string): Node {
+        return this.owners.get(name) || (this.parent && this.parent.getOwner(name));
     }
 
     getInit(name: string): Identifier {
         return this.inits.get(name) || this.parent?.getInit(name);
     }
 
-	isLet(name: string) {
-		const owner = this.getOwner(name);
-		return owner && (owner.type === 'Element' || owner.type === 'InlineComponent');
-	}
+    isLet(name: string) {
+        const owner = this.getOwner(name);
+        return owner && (owner.type === 'Element' || owner.type === 'InlineComponent');
+    }
 }
