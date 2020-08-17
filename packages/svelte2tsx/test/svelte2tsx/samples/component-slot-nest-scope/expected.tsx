50d83c776a6989323c2932c16440bed14352d4d4
Lyu Jason <jason.lyu@supergeo.com.tw>
2020-08-17 18:57:33 +0800

    test for nest

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/component-slot-nest-scope/expected.tsx
@@ -0,0 +1,11 @@
+///<reference types="svelte" />
+<></>;function render() {
+<>{__sveltets_each(items, (item) => <>
+    {__sveltets_each(item, ({ a }) => <>
+        <slot a={a}>Hello</slot>
+    </>)}
+</>)}</>
+return { props: {}, slots: {default: {a:(({ a }) => a)(__sveltets_unwrapArr(__sveltets_unwrapArr(items)))}}, getters: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(render)) {
+}
---
+++ packages/svelte2tsx/test/svelte2tsx/samples/component-slot-nest-scope/input.svelte
@@ -0,0 +1,5 @@
+{#each items as item}
+    {#each item as { a }}
+        <slot {a}>Hello</slot>
+    {/each}
+{/each}
