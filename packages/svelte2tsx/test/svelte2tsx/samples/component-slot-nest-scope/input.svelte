699818602604350c1ea1ef7a25916bc66cf01a74
Lyu Jason <b00605009@ntu.edu.tw>
2020-08-23 17:19:53 +0800

    more test about scope

--- packages/svelte2tsx/test/svelte2tsx/samples/component-slot-nest-scope/input.svelte
+++ packages/svelte2tsx/test/svelte2tsx/samples/component-slot-nest-scope/input.svelte
@@ -2,4 +2,10 @@
     {#each item as { a }}
         <slot {a}>Hello</slot>
     {/each}
+    <slot name="second" { a }></slot>
 {/each}
+<Component let:c>{ c }</Component>
+{#await promise then d}
+    {d}
+{/await}
+<slot name="third" {d} {c}></slot>
