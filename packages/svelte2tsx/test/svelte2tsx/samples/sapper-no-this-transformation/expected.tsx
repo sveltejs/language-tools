a42c674dc1b376d1692d636fa098facf7b9eba1d
Simon Holthausen <simon.holthausen@accso.de>
2020-08-29 14:50:17 +0200

    test for no-this-transformations

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/sapper-no-this-transformation/expected.tsx
@@ -0,0 +1,21 @@
+///<reference types="svelte" />
+<></>;
+    this.shouldNotBeTransformed;
+    export async function notPreload() {
+        this.shouldNotBeTransformed;
+    }
+;<></>;function render() {
+
+    this.shouldNotBeTransformed;
+     async function preload() {
+        this.shouldNotBeTransformed;
+    }
+;
+() => (<>
+
+</>);
+return { props: {}, slots: {}, getters: {preload: preload}, events: {} }}
+
+export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+    get preload() { return render().getters.preload }
+}
---
+++ packages/svelte2tsx/test/svelte2tsx/samples/sapper-no-this-transformation/input.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+    this.shouldNotBeTransformed;
+    export async function notPreload() {
+        this.shouldNotBeTransformed;
+    }
+</script>
+
+<script>
+    this.shouldNotBeTransformed;
+    export async function preload() {
+        this.shouldNotBeTransformed;
+    }
+</script>
