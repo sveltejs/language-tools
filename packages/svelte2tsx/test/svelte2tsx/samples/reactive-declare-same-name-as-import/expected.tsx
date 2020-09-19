ce3679ef23cdb122933a00a66433a269d29aac48
Simon H <5968653+dummdidumm@users.noreply.github.com>
2020-09-19 16:46:35 +0200

    (fix) don't track propertyName of named import alias (#551)
    #549
    Don't track `a` in `import {a as b} from ..` as a declared variable

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/reactive-declare-same-name-as-import/expected.tsx
@@ -0,0 +1,16 @@
+///<reference types="svelte" />
+<></>;
+import { b as c } from "foo";
+function render() {
+
+
+let  b = __sveltets_invalidate(() => 7);
+
+let a;
+$: a = __sveltets_invalidate(() => 5);
+;
+() => (<></>);
+return { props: {}, slots: {}, getters: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+}
