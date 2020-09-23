92d7ca445261f4469d0bb7842afcaa49db351fab
Jérémie <jeremie.brebec@gmail.com>
2020-09-23 19:03:59 +0200

    Fix reactive break #559 (#562)

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/reactive-break-$/expected.tsx
@@ -0,0 +1,10 @@
+///<reference types="svelte" />
+<></>;function render() {
+
+    ;() => {$: { break $; }}
+;
+() => (<></>);
+return { props: {}, slots: {}, getters: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+}
