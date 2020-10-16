de428861f2b744da95a5f090905dfa97164da8e6
Simon Holthausen <simon.holthausen@accso.de>
2020-10-16 15:09:02 +0200

    (fix) filter out invalid class name chars
    #613

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/filename-is-invalid-identifier/expected.tsx
@@ -0,0 +1,7 @@
+///<reference types="svelte" />
+<></>;function render() {
+<></>
+return { props: {}, slots: {}, getters: {}, events: {} }}
+
+export default class AslugTest__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+}
