8ecdc7df7f2da5e5f51485d7294c65c2baefbb3e
Simon H <5968653+dummdidumm@users.noreply.github.com>
2020-11-05 20:22:24 +0100

    (fix) blank content method (#656)
    #655

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-style-and-script/expected.tsx
@@ -0,0 +1,11 @@
+///<reference types="svelte" />
+<></>;function render() {
+
+     let foo:string;
+;
+() => (<>
+</>);
+return { props: {foo: foo} as {foo: string}, slots: {}, getters: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+}
