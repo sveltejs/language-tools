34903a4e1e32970678a1dc8ed430a70882fed2b0
Lyu, Wei-Da <36730922+jasonlyu123@users.noreply.github.com>
2020-11-18 23:12:35 +0800

    (fix) svelte2tsx: transform type assertion (#679)
    #477
    transform type assertion to "as" expression

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-type-assertion/expected.tsx
@@ -0,0 +1,10 @@
+///<reference types="svelte" />
+<></>;function render() {
+
+    let a = document.querySelector('#id') as HTMLInputElement;
+;
+() => (<></>);
+return { props: {}, slots: {}, getters: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+}
