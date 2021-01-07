2999ce69968009d8b625a0832cb7f6171720f657
pushkine <pushkine.github@gmail.com>
2021-01-07 19:46:50 +0100

    init

--- packages/svelte2tsx/src/htmlxtojsx/nodes/debug.ts
+++ packages/svelte2tsx/src/htmlxtojsx/nodes/debug.ts
@@ -1,10 +1,20 @@
-import MagicString from 'magic-string';
 import { Node } from 'estree-walker';
+import MagicString from 'magic-string';
 
 /**
- * {@debug ...}   --->   {...}
+ * {@debug a}		--->   {a}
+ *
+ * tsx won't accept commas, must split
+ * {@debug a, b}	--->   {a}{b}
  */
 export function handleDebug(htmlx: string, str: MagicString, debugBlock: Node): void {
-    const tokenStart = htmlx.indexOf('@debug', debugBlock.start);
-    str.remove(tokenStart, tokenStart + '@debug'.length);
+    const content = htmlx.slice(debugBlock.start + '@debug'.length + 1, debugBlock.end);
+    str.overwrite(
+        debugBlock.start,
+        debugBlock.end,
+        content
+            .split(',')
+            .map((v) => `{${v.trim()}}`)
+            .join('')
+    );
 }
