582b9a3ca6363f3b30a83f1e30a26459bcff18de
Bob Fanger <bfanger@gmail.com>
2021-02-14 13:23:54 +0100

    fix: shorthand transition and animation params
    Fixes #785

--- packages/svelte2tsx/test/htmlx2jsx/samples/animation-bare/expected.jsx
+++ packages/svelte2tsx/test/htmlx2jsx/samples/animation-bare/expected.jsx
@@ -1,1 +1,1 @@
-<><h1 {...__sveltets_ensureAnimation(blink(__sveltets_mapElementTag('h1'),__sveltets_AnimationMove))}>Hello</h1></>
+<><h1 {...__sveltets_ensureAnimation(blink(__sveltets_mapElementTag('h1'),__sveltets_AnimationMove,{}))}>Hello</h1></>
