4e58599021af6870a221c8d2d2af55ab5fbeb6ca
Simon Holthausen <simon.holthausen@accso.de>
2021-02-21 12:59:15 +0100

    (fix) correct multiple slot let transformation
    Parantheses are important..
    #834

--- packages/svelte2tsx/test/htmlx2jsx/samples/component-multi-slot/expected.jsx
+++ packages/svelte2tsx/test/htmlx2jsx/samples/component-multi-slot/expected.jsx
@@ -4,6 +4,7 @@
         <h2>Hi Slot {newvar}</h2>
     </div></>}}
     {() => { let {newvar2} = __sveltets_instanceOf(Component).$$slot_def['slotwithoutchildren'];<><div   {...__sveltets_ensureType(Boolean, !!(newvar2))}></div></>}}
+    {() => { let {hi1, hi2, hi3:hi3alias} = __sveltets_instanceOf(Component).$$slot_def['slotwithmultiplelets'];<><div    ></div></>}}
     <p >
         Test
     </p>
