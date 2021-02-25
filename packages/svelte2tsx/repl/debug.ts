d6ee63f7de06ed8496167796b99a81ab1c4a0dab
pushkine <pushkine.github@gmail.com>
2021-02-25 06:53:17 +0100

    prettier

--- packages/svelte2tsx/repl/debug.ts
+++ packages/svelte2tsx/repl/debug.ts
@@ -4,10 +4,10 @@ const content = fs.readFileSync(`${__dirname}/index.svelte`, 'utf-8');
 svelte2tsx(content);
 /**
  * To enable the REPL, simply run the "dev" package script.
- * 
- * The "/repl/index.svelte" file will be converted to tsx 
- * at "repl/output/" using the modified source code on change.
- * 
+ *
+ * The "/repl/index.svelte" file will be converted to tsx
+ * at "/repl/output/" using the modified source code on change.
+ *
  * Alternatively you may run this file with a debugger attached,
  * to do so, hit "Ctrl+Shift+D" and select "svelte2tsx" in the dropdown.
  */
