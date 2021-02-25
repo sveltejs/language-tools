5d94d7f0fef9db72a0f885608bbc89c9fb20a29d
pushkine <pushkine.github@gmail.com>
2021-02-25 06:49:28 +0100

    remove sucrase

--- packages/svelte2tsx/repl/debug.ts
+++ packages/svelte2tsx/repl/debug.ts
@@ -2,13 +2,13 @@ import fs from 'fs';
 import svelte2tsx from '../src';
 const content = fs.readFileSync(`${__dirname}/index.svelte`, 'utf-8');
 svelte2tsx(content);
-
+debugger;
 /**
- * Run the "dev" package script to get started
- *
- * Inputs at "repl/index.svelte" use the modified source code
- * and are compiled to "repl/output/" on save.
- *
- * Alternatively, attach a debugger to the "debug" package script
- * and place breakpoints directly in the ts source files.
+ * To enable the REPL, simply run the "dev" package script.
+ * 
+ * The "/repl/index.svelte" file will be converted to tsx 
+ * at "repl/output/" using the modified source code on change.
+ * 
+ * Alternatively you may run this file with a debugger attached,
+ * to do so, hit "Ctrl+Shift+D" and select "svelte2tsx" in the dropdown.
  */
