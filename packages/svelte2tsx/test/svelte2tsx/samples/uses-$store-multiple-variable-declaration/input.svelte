469b5d98eb6a3d92b58f6d778a104475ed55e14d
Simon Holthausen <simon.holthausen@accso.de>
2021-03-04 16:38:13 +0100

    enhance test

--- packages/svelte2tsx/test/svelte2tsx/samples/uses-$store-multiple-variable-declaration/input.svelte
+++ packages/svelte2tsx/test/svelte2tsx/samples/uses-$store-multiple-variable-declaration/input.svelte
@@ -1,8 +1,8 @@
 <script>
     const store1 = '', store2 = '';
-    const { store3 } = '', [ store ] = '';
-    $: ({store5} = '');
-    $: [store6] = '';
+    const { store3, store4 } = '', [ store5, store6 ] = '';
+    $: ({store7, store8} = '');
+    $: [store9, store10] = '';
 </script>
 
 {$store1}
@@ -11,3 +11,7 @@
 {$store4}
 {$store5}
 {$store6}
+{$store7}
+{$store8}
+{$store9}
+{$store10}
