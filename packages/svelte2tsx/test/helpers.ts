e22abb8346293c3487ad8af4074199ee677fb0e6
pushkine <pushkine.github@gmail.com>
2021-03-09 23:13:05 +0100

    Update helpers.ts

--- packages/svelte2tsx/test/helpers.ts
+++ packages/svelte2tsx/test/helpers.ts
@@ -1,5 +1,5 @@
-import assert from 'assert';
 import fs from 'fs';
+import assert from 'assert';
 import { TestFunction } from 'mocha';
 import svelte2tsx from './build/index';
 import { htmlx2jsx } from './build/htmlxtojsx';
@@ -12,6 +12,92 @@ function readFileSync(path: string) {
     return fs.existsSync(path)
         ? fs.readFileSync(path, 'utf-8').replace(/\r\n/g, '\n').replace(/\s+$/, '')
         : null;
+}
+
+class Sample {
+    check_dir({ required = [], allowed = required }: { allowed?: string[]; required?: string[] }) {
+        const unchecked = new Set(required);
+        const unknown = [];
+        loop: for (const fileName of this.folder) {
+            for (const name of unchecked) {
+                if ('*' === name[0] ? fileName.endsWith(name.slice(1)) : name === fileName) {
+                    unchecked.delete(name);
+                    continue loop;
+                }
+            }
+            for (const name of allowed) {
+                if ('*' === name[0] ? fileName.endsWith(name.slice(1)) : name === fileName) {
+                    continue loop;
+                }
+            }
+            unknown.push(fileName);
+        }
+        if (unknown.length) {
+            const errors =
+                unknown.map((name) => `Unexpected file "${name}"`).join('\n') +
+                `\nat ${this.directory}`;
+            if (process.env.CI) {
+                throw new Error('\n' + errors);
+            } else {
+                after(() => {
+                    console.log(errors);
+                });
+            }
+        }
+        if (unchecked.size) {
+            throw new Error(
+                `Expected file${unchecked.size === 1 ? '' : 's'} ${[...unchecked]
+                    .map((str) => `"${str}"`)
+                    .join(', ')} in "${this.directory}"`
+            );
+        }
+    }
+    readonly folder: string[];
+    readonly directory: string;
+    constructor(dir: string, readonly name: string) {
+        this.directory = `${dir}/samples/${name}`;
+        this.folder = fs.readdirSync(this.directory);
+    }
+    it(fn: () => void) {
+        let _it = it;
+        if (this.name.startsWith('.')) {
+            _it = it.skip as TestFunction;
+        } else if (this.name.endsWith('.solo')) {
+            _it = it.only as TestFunction;
+        }
+        const sample = this;
+        _it(this.name, function () {
+            try {
+                fn();
+                if (sample.skipped) this.skip();
+            } catch (err) {
+                if (sample.skipped) this.skip();
+                throw err;
+            }
+        });
+    }
+    has(file: string) {
+        return this.folder.includes(file);
+    }
+    get(file: string) {
+        return readFileSync(`${this.directory}/${file}`);
+    }
+    private skipped = false;
+    generate(fileName: string, content: string) {
+        const path = `${this.directory}/${fileName}`;
+        if (process.env.CI) {
+            throw new Error(`Forgot to generate expected sample result at "${path}"`);
+        }
+        after(() => {
+            fs.writeFileSync(path, content);
+            console.info(`(generated) ${this.name}/${fileName}`);
+        });
+        this.skipped = true;
+    }
+    eval(fileName: string, ...args: any[]) {
+        const fn = require(`${this.directory}/${fileName}`);
+        fn(...args);
+    }
 }
 type TransformSampleFn = (
     input: string,
@@ -26,7 +112,7 @@ export function test_samples(dir: string, transform: TransformSampleFn, jsx: 'js
     for (const sample of each_sample(dir)) {
         const svelteFile = sample.folder.find((f) => f.endsWith('.svelte'));
 
-        sample.check({
+        sample.check_dir({
             required: ['*.svelte'],
             allowed: ['expected.js', `expected.${jsx}`, 'expected.error.json']
         });
@@ -78,91 +164,7 @@ export function test_samples(dir: string, transform: TransformSampleFn, jsx: 'js
         });
     }
 }
-class Sample {
-    readonly folder: string[];
-    readonly directory: string;
-    constructor(dir: string, readonly name: string) {
-        this.directory = `${dir}/samples/${name}`;
-        this.folder = fs.readdirSync(this.directory);
-    }
-    it(fn: () => void) {
-        let _it = it;
-        if (this.name.startsWith('.')) {
-            _it = it.skip as TestFunction;
-        } else if (this.name.endsWith('.solo')) {
-            _it = it.only as TestFunction;
-        }
-        const sample = this;
-        _it(this.name, function () {
-            try {
-                fn();
-                if (sample.skipped) this.skip();
-            } catch (err) {
-                if (sample.skipped) this.skip();
-                throw err;
-            }
-        });
-    }
-    has(file: string) {
-        return this.folder.includes(file);
-    }
-    get(file: string) {
-        return readFileSync(`${this.directory}/${file}`);
-    }
-    private skipped = false;
-    generate(fileName: string, content: string) {
-        const path = `${this.directory}/${fileName}`;
-        if (process.env.CI) {
-            throw new Error(`Forgot to generate expected sample result at "${path}"`);
-        }
-        after(() => {
-            fs.writeFileSync(path, content);
-            console.info(`(generated) ${this.name}/${fileName}`);
-        });
-        this.skipped = true;
-    }
-    eval(fileName: string, ...args: any[]) {
-        const fn = require(`${this.directory}/${fileName}`);
-        fn(...args);
-    }
-    check({ required = [], allowed = required }: { allowed?: string[]; required?: string[] }) {
-        const unchecked = new Set(required);
-        const unknown = [];
-        loop: for (const fileName of this.folder) {
-            for (const name of unchecked) {
-                if ('*' === name[0] ? fileName.endsWith(name.slice(1)) : name === fileName) {
-                    unchecked.delete(name);
-                    continue loop;
-                }
-            }
-            for (const name of allowed) {
-                if ('*' === name[0] ? fileName.endsWith(name.slice(1)) : name === fileName) {
-                    continue loop;
-                }
-            }
-            unknown.push(fileName);
-        }
-        if (unknown.length) {
-            const errors =
-                unknown.map((name) => `Unexpected file "${name}"`).join('\n') +
-                `\nat ${this.directory}`;
-            if (process.env.CI) {
-                throw new Error('\n' + errors);
-            } else {
-                after(() => {
-                    console.log(errors);
-                });
-            }
-        }
-        if (unchecked.size) {
-            throw new Error(
-                `Expected file${unchecked.size === 1 ? '' : 's'} ${[...unchecked]
-                    .map((str) => `"${str}"`)
-                    .join(', ')} in "${this.directory}"`
-            );
-        }
-    }
-}
+
 export function* each_sample(dir: string) {
     for (const name of fs.readdirSync(`${dir}/samples`)) {
         yield new Sample(dir, name);
