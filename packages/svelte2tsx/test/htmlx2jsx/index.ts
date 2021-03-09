68fa7c28f4b6e83fd421e461dacbc86e639c36e7
pushkine <pushkine.github@gmail.com>
2021-03-09 22:01:56 +0100

    Update index.ts

--- packages/svelte2tsx/test/htmlx2jsx/index.ts
+++ packages/svelte2tsx/test/htmlx2jsx/index.ts
@@ -2,5 +2,5 @@ import { htmlx2jsx } from '../build/htmlxtojsx';
 import { test_samples } from '../helpers';
 
 describe('htmlx2jsx', () => {
-	test_samples(__dirname, htmlx2jsx, 'jsx');
+    test_samples(__dirname, htmlx2jsx, 'jsx');
 });
