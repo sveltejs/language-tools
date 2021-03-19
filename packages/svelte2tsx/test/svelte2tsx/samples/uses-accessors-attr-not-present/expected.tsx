2f5badb8e38ee7aac8da712ae7d1c0e099f36a9b
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-03-19 12:36:25 +0100

    Update 'use-accessors' tests, see https://github.com/sveltejs/language-tools/issues/882#issuecomment-802661118
    - setters with brackets
    - `export const bar` don't create setters
    
    All 217 test are passing.

--- packages/svelte2tsx/test/svelte2tsx/samples/uses-accessors-attr-not-present/expected.tsx
+++ packages/svelte2tsx/test/svelte2tsx/samples/uses-accessors-attr-not-present/expected.tsx
@@ -2,10 +2,12 @@
 <></>;function render() {
 
 	 let foo: number = undefined;foo = __sveltets_any(foo);
+	 const bar: string = ''
 ;
 () => (<><svelteoptions />
 </>);
-return { props: {foo: foo}, slots: {}, getters: {}, setters: {}, events: {} }}
+return { props: {foo: foo , bar: bar}, slots: {}, getters: {bar: bar}, setters: {}, events: {} }}
 
 export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+    get bar() { return render().getters.bar }
 }
--- packages/svelte2tsx/test/svelte2tsx/samples/uses-accessors-mustachetag-true/expected.tsx
+++ packages/svelte2tsx/test/svelte2tsx/samples/uses-accessors-mustachetag-true/expected.tsx
@@ -2,14 +2,16 @@
 <></>;function render() {
 
 	 let foo: number = undefined;foo = __sveltets_any(foo);
+	 const bar: string = ''
 ;
 () => (<><svelteoptions accessors={true} />
 </>);
-return { props: {foo: foo}, slots: {}, getters: {foo: foo}, setters: {foo: foo}, events: {} }}
+return { props: {foo: foo , bar: bar}, slots: {}, getters: {foo: foo, bar: bar}, setters: {foo: foo}, events: {} }}
 
 export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
     get foo() { return render().getters.foo }
+    get bar() { return render().getters.bar }
 
     /**accessor*/
-    set foo(foo)
+    set foo(foo) {}
 }
