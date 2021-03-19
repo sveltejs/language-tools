f33173f933908dd36c3c0282a61676be92ba1bb3
Simon H <5968653+dummdidumm@users.noreply.github.com>
2021-03-19 12:48:33 +0100

    (chore) test for #886 (#892)

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/event-and-forwarded-event/expected.tsx
@@ -0,0 +1,17 @@
+///<reference types="svelte" />
+<></>;
+import { createEventDispatcher } from "svelte";
+function render() {
+
+    
+
+    const dispatch = createEventDispatcher();
+    dispatch("mount", { input });
+;
+() => (<>
+
+<input onfocus={undefined} /></>);
+return { props: {}, slots: {}, getters: {}, events: {'focus':__sveltets_mapElementEvent('focus'), 'mount': __sveltets_customEvent} }}
+
+export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+}
