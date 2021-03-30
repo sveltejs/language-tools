4d2b09816c0b85b7a10332ab570bfcb3a4add926
pushkine <pushkine.github@gmail.com>
2021-03-30 12:12:39 +0200

    (chore) Svelte2tsx SourceMapping tests rewrite (#864)
    Changed
    - Relocated samples to samples/{name}. Relocated svelte input to input.svelte, relocated tests to test.jsx
    - Added source-map-codec to devDependencies to decode mappings
    
    Added
    - Added auto-generating file mappings.jsx, it contains the generated output with a comment under each line describing each character's mapping and how they map back to the origin. The intent of that file is to highlight changes on diffchecks
    - Added auto-generating untracked file test.edit.jsx, it mirrors the ranges tested in test.jsx in the form of the generated output with range positions surrounded by triple brackets. Editing this file and running tests will regenerate new tests based on those ranges
    - Added auto-generating untracked file output.tsx, It contains the generated output with its inlined sourcemap. That file can be directly uploaded to online sourcemapping tools, e.g. https://evanw.github.io/source-map-visualization/

---
+++ packages/svelte2tsx/test/sourcemaps/samples/property-shorthand/mappings.jsx
@@ -0,0 +1,13 @@
+///<reference types="svelte" />
+<></>;function render() {                                                                                                                             {/**
+------------------------------------------------------------------------------------------------------------------------------------------------------ */}
+<><button count={count}>button</button></>                                                                                                            {/**
+=#                                             Originless mappings                                                                                    
+<><button•count={count}>button</button></>↲    [generated] line 3                                                                                     
+  <button•      {count}>button</button>                                                                                                               
+<button•{count}>button</button>                [original] line 1                                                                                      
+------------------------------------------------------------------------------------------------------------------------------------------------------ */}
+return { props: {}, slots: {}, getters: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends createSvelte2TsxComponent(__sveltets_partial(__sveltets_with_any_event(render))) {
+}
