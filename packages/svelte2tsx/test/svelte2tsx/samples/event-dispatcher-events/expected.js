8747c5576309f4a616a5dcbb00ade85efe6862df
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-04-16 21:11:55 +0700

    add formatted expected.js files

--- packages/svelte2tsx/test/svelte2tsx/samples/event-dispatcher-events/expected.js
+++ packages/svelte2tsx/test/svelte2tsx/samples/event-dispatcher-events/expected.js
@@ -1,9 +1,9 @@
 let assert = require('assert');
 
 module.exports = function ({ events }) {
-    assert.deepEqual(events.getAll(), [
-        { name: 'btn', type: 'CustomEvent<any>' },
-        { name: 'hi', type: 'CustomEvent<any>' },
-        { name: 'bye', type: 'CustomEvent<any>' }
-    ]);
+	assert.deepEqual(events.getAll(), [
+		{ name: 'btn', type: 'CustomEvent<any>' },
+		{ name: 'hi', type: 'CustomEvent<any>' },
+		{ name: 'bye', type: 'CustomEvent<any>' }
+	]);
 };
