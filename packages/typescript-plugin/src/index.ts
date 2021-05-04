505d7dcdcda88f69d99efb34047bfaa8950ae52c
Simon H <5968653+dummdidumm@users.noreply.github.com>
2021-05-04 17:34:10 +0200

    (feat) TypeScript plugin (#978)
    Initially support
    - rename (doesn't work for prop renames yet)
    - diagnostics
    - find references
    - go to definition
    - go to implementation(s)
    
    This makes all files TSX hardcoded for now, it seems the TS language server is okay with importing tsx into js
    
    #580
    #550
    #342
    #110

--- packages/typescript-plugin/src/index.ts
+++ packages/typescript-plugin/src/index.ts
@@ -1,10 +1,56 @@
-function init(modules: { typescript: typeof import('typescript/lib/tsserverlibrary') }) {
+import { dirname, resolve } from 'path';
+import { decorateLanguageService } from './language-service';
+import { Logger } from './logger';
+import { patchModuleLoader } from './module-loader';
+import { SvelteSnapshotManager } from './svelte-snapshots';
+import type ts from 'typescript/lib/tsserverlibrary';
+
+function init(modules: { typescript: typeof ts }) {
     function create(info: ts.server.PluginCreateInfo) {
-        // TODO
+        const logger = new Logger(info.project.projectService.logger);
+        logger.log('Starting Svelte plugin');
+
+        const snapshotManager = new SvelteSnapshotManager(
+            modules.typescript,
+            info.project.projectService,
+            logger,
+            !!info.project.getCompilerOptions().strict
+        );
+
+        patchCompilerOptions(info.project);
+        patchModuleLoader(
+            logger,
+            snapshotManager,
+            modules.typescript,
+            info.languageServiceHost,
+            info.project
+        );
+        return decorateLanguageService(info.languageService, snapshotManager, logger);
     }
 
     function getExternalFiles(project: ts.server.ConfiguredProject) {
-        // TODO
+        // Needed so the ambient definitions are known inside the tsx files
+        const svelteTsPath = dirname(require.resolve('svelte2tsx'));
+        const svelteTsxFiles = [
+            './svelte-shims.d.ts',
+            './svelte-jsx.d.ts',
+            './svelte-native-jsx.d.ts'
+        ].map((f) => modules.typescript.sys.resolvePath(resolve(svelteTsPath, f)));
+        return svelteTsxFiles;
+    }
+
+    function patchCompilerOptions(project: ts.server.Project) {
+        const compilerOptions = project.getCompilerOptions();
+        // Patch needed because svelte2tsx creates jsx/tsx files
+        compilerOptions.jsx = modules.typescript.JsxEmit.Preserve;
+
+        // detect which JSX namespace to use (svelte | svelteNative) if not specified or not compatible
+        if (!compilerOptions.jsxFactory || !compilerOptions.jsxFactory.startsWith('svelte')) {
+            // Default to regular svelte, this causes the usage of the "svelte.JSX" namespace
+            // We don't need to add a switch for svelte-native because the jsx is only relevant
+            // within Svelte files, which this plugin does not deal with.
+            compilerOptions.jsxFactory = 'svelte.createElement';
+        }
     }
 
     return { create, getExternalFiles };
---
+++ packages/typescript-plugin/src/language-service/definition.ts
@@ -0,0 +1,44 @@
+import type ts from 'typescript/lib/tsserverlibrary';
+import { Logger } from '../logger';
+import { SvelteSnapshotManager } from '../svelte-snapshots';
+import { isNotNullOrUndefined, isSvelteFilePath } from '../utils';
+
+export function decorateGetDefinition(
+    ls: ts.LanguageService,
+    snapshotManager: SvelteSnapshotManager,
+    logger: Logger
+): void {
+    const getDefinitionAndBoundSpan = ls.getDefinitionAndBoundSpan;
+    ls.getDefinitionAndBoundSpan = (fileName, position) => {
+        const definition = getDefinitionAndBoundSpan(fileName, position);
+        if (!definition?.definitions) {
+            return definition;
+        }
+
+        return {
+            ...definition,
+            definitions: definition.definitions
+                .map((def) => {
+                    if (!isSvelteFilePath(def.fileName)) {
+                        return def;
+                    }
+
+                    const textSpan = snapshotManager
+                        .get(def.fileName)
+                        ?.getOriginalTextSpan(def.textSpan);
+                    if (!textSpan) {
+                        return undefined;
+                    }
+                    return {
+                        ...def,
+                        textSpan,
+                        // Spare the work for now
+                        originalTextSpan: undefined,
+                        contextSpan: undefined,
+                        originalContextSpan: undefined
+                    };
+                })
+                .filter(isNotNullOrUndefined)
+        };
+    };
+}
