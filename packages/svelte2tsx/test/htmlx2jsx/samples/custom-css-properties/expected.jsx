3c4fe63b45adb3300ce7e95c3c2802175f8a4fc9
Simon H <5968653+dummdidumm@users.noreply.github.com>
2021-05-24 12:45:45 +0200

    (fix) better transform of custom css properties (#1024)
    Transform instead of delete to ensure better DX (correct mappings) and to not break if the property value does contain for example a store which leads to a inner transform.
    #1023
    #974

--- packages/svelte2tsx/test/htmlx2jsx/samples/custom-css-properties/expected.jsx
+++ packages/svelte2tsx/test/htmlx2jsx/samples/custom-css-properties/expected.jsx
@@ -1,1 +1,7 @@
-<><Component   betweenprop   /></>
+<><Component 
+  {...__sveltets_cssProp({"--custom-css-property1": `${'hi'}`})}
+  {...__sveltets_cssProp({"--custom-css-property2": `hi`})}
+  betweenprop
+  {...__sveltets_cssProp({"--custom-css-property3": `hi${jo}hi`})}
+  {...__sveltets_cssProp({"--custom-css-property4": `hi${jo}hi`})}
+  {...__sveltets_cssProp({"--custom-css-property5": `hi${jo}hi`})} /></>
