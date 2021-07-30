b5ff57c7f00ecad6156386085d76c0f1f061ca42
Lyu, Wei Da <b00605009@ntu.edu.tw>
2021-07-30 14:22:40 +0800

    workaround in svelte2tsx otherwise ts would think the node span over multiple lines

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/import-equal/expected.tsx
@@ -0,0 +1,11 @@
+///<reference types="svelte" />
+<></>;function render() {
+
+    import A;
+    import C = require('');
+;
+() => (<></>);
+return { props: {}, slots: {}, getters: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends __sveltets_1_createSvelte2TsxComponent(__sveltets_1_partial(__sveltets_1_with_any_event(render()))) {
+}
