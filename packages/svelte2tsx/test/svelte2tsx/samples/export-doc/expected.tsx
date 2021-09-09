6300670232266e9bbd3eb39af71683a147ccecb3
Simon H <5968653+dummdidumm@users.noreply.github.com>
2021-09-09 10:58:40 +0200

    (fix) fix doc for comma separated exports (#1169)
    #1168

--- packages/svelte2tsx/test/svelte2tsx/samples/export-doc/expected.tsx
+++ packages/svelte2tsx/test/svelte2tsx/samples/export-doc/expected.tsx
@@ -13,6 +13,14 @@
      */
      let b;
      let c;
+    /**
+     * d
+     */
+     let d;let 
+    /**
+     * e
+     */
+     e;
 ;
 () => (<></>);
 return { props: {
@@ -21,7 +29,13 @@ return { props: {
      */a: a , 
 /**
      * MORE DOCS!
-     */b: b , c: c}, slots: {}, getters: {}, events: {} }}
+     */b: b , c: c , 
+/**
+     * d
+     */d: d , 
+/**
+     * e
+     */e: e}, slots: {}, getters: {}, events: {} }}
 
 export default class Input__SvelteComponent_ extends __sveltets_1_createSvelte2TsxComponent(__sveltets_1_partial(__sveltets_1_with_any_event(render()))) {
 }
