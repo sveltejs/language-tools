5f4342cc0c74f74e1735ed209312f78b059b96fe
Simon Holthausen <simon.holthausen@accso.de>
2021-11-29 10:26:11 +0100

    (chore) refactor diagnostic tests
    Create fixtures for more consistent testing and easier setup of new tests

--- packages/language-server/test/plugins/typescript/features/diagnostics/fixtures/accessors-customElement-configs/input.svelte
+++ packages/language-server/test/plugins/typescript/features/diagnostics/fixtures/accessors-customElement-configs/input.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import AccessorsAndOption from './acceesors/accessors-and-option.svelte';
-    import Accessors from './acceesors/accessors.svelte';
+    import AccessorsAndOption from './accessors/accessors-and-option.svelte';
+    import Accessors from './accessors/accessors.svelte';
     import CustomElement from './customElement/customElement.svelte';
 
     let c: CustomElement;
