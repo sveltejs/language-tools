d6dcfbfe344865f02f4b761d34c43dc272d4dbb8
Lyu, Wei Da <b00605009@ntu.edu.tw>
2022-02-09 11:22:48 +0800

    manual overwrite html tag for better mapping

--- packages/svelte2tsx/src/htmlxtojsx_v2/nodes/RawMustacheTag.ts
+++ packages/svelte2tsx/src/htmlxtojsx_v2/nodes/RawMustacheTag.ts
@@ -1,13 +1,15 @@
 import MagicString from 'magic-string';
 import { BaseNode } from '../../interfaces';
-import { getNodeRangeIncludingTrailingPropertyAccess, transform } from '../utils/node-utils';
+import { getNodeEndIncludingTrailingPropertyAccess } from '../utils/node-utils';
 
 /**
  * {@html ...}   --->   ...;
  */
 export function handleRawHtml(str: MagicString, node: BaseNode): void {
-    transform(str, node.start, node.end, node.end, [
-        getNodeRangeIncludingTrailingPropertyAccess(str.original, node.expression),
+    str.overwrite(node.start, node.expression.start, ' ');
+    str.overwrite(
+        getNodeEndIncludingTrailingPropertyAccess(str.original, node.expression.end),
+        node.end,
         ';'
-    ]);
+    );
 }
