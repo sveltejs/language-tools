3e62ec0e49415c0c94ce9059efb31e5b193d74ad
Lyu, Wei Da <b00605009@ntu.edu.tw>
2022-02-10 15:02:28 +0800

    (fix) svelte2tsx function expression scope

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/reactive-declare-same-name-as-function-parameter/input.svelte
@@ -0,0 +1,11 @@
+<script>
+const a = function (shadowed1) {}
+const b = (shadowed2) => {}
+$: c = function (shadowed3) {}
+$: d = (shadowed4) => {}
+
+$: shadowed1 = 1
+$: shadowed2 = 1
+$: shadowed3 = 1
+$: shadowed4 = 1
+</script>
