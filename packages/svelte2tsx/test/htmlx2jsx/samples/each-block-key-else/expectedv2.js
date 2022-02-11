0c0186cfc27bc155a7bc84dcb089a927fb72c389
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-02-11 17:29:45 +0100

    (fix) add key after index definition in new each transformation (#1385)
    #1352

--- packages/svelte2tsx/test/htmlx2jsx/samples/each-block-key-else/expectedv2.js
+++ packages/svelte2tsx/test/htmlx2jsx/samples/each-block-key-else/expectedv2.js
@@ -1,4 +1,4 @@
-    for(const item of __sveltets_2_ensureArray(items)){item.id;let i = 1;
+    for(const item of __sveltets_2_ensureArray(items)){let i = 1;item.id;
      { svelteHTML.createElement("div", {});item;i; }
 }
      { svelteHTML.createElement("div", {});  }
