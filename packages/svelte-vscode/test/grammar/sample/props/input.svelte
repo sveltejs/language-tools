c166db06c8afb732eba79660e8a83e98c0e95975
Lyu, Wei Da <b00605009@ntu.edu.tw>
2022-02-23 13:54:51 +0800

    more test from past issues

--- packages/svelte-vscode/test/grammar/sample/props/input.svelte
+++ packages/svelte-vscode/test/grammar/sample/props/input.svelte
@@ -3,6 +3,8 @@
 <input {value}>
 <input value="{1}">
 
+<input type=number min=0>
+
 <div tabindex="1" />
 <div tabindex={1} />
 <div {tabindex} />
@@ -12,3 +14,8 @@
 <Input value="1" />
 <Input value={1} />
 <Input {value} />
+
+<div title="Really?
+    {true ? 'Yes' : 'No'}"></div>
+
+<Input hi_hi={false} hi-hi="" />
