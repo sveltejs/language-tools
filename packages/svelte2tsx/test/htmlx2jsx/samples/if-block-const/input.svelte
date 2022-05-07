327fc25ab6b48db990aa8eca2c3643f1a561b78a
Simon <simon.holthausen@accso.de>
2022-05-07 11:02:49 +0200

    (chore) bump Svelte version
    support for const in if blocks in new transformation
    #1470

---
+++ packages/svelte2tsx/test/htmlx2jsx/samples/if-block-const/input.svelte
@@ -0,0 +1,10 @@
+{#if name == "world"}
+    {@const hello = name}
+    <h1>Hello {hello}</h1>
+{:else if true}
+    {@const hello = name}
+    <h1>Hello {hello}</h1>
+{:else}
+    {@const hello = name}
+    <h1>Hello {hello}</h1>
+{/if}
