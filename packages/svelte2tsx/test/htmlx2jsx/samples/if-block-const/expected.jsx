bcf6b6a0202758080c101c00bb0442f0f53d95ff
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-05-10 15:01:07 +0200

    (chore) bump Svelte version (#1474)
    support for const in if blocks in new transformation
    #1470

---
+++ packages/svelte2tsx/test/htmlx2jsx/samples/if-block-const/expected.jsx
@@ -0,0 +1,10 @@
+<>{(name == "world") ? <>
+    {@const hello = name}
+    <h1>Hello {hello}</h1>
+</> : (true) ? <>
+    {@const hello = name}
+    <h1>Hello {hello}</h1>
+</> : <>
+    {@const hello = name}
+    <h1>Hello {hello}</h1>
+</> }</>
---
+++ packages/svelte2tsx/test/htmlx2jsx/samples/if-block-const/expectedv2.js
@@ -0,0 +1,10 @@
+if(name == "world"){
+    const hello = name;
+     { svelteHTML.createElement("h1", {}); hello; }
+} else if (true){
+    const hello = name;
+     { svelteHTML.createElement("h1", {}); hello; }
+}else{
+    const hello = name;
+     { svelteHTML.createElement("h1", {}); hello; }
+}
