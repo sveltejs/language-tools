db20058afea1d52cbfbf434b56301382dc827e50
Lyu, Wei-Da <36730922+jasonlyu123@users.noreply.github.com>
2022-05-13 22:14:07 +0800

    (feat) support const tag in if block in the old transformation (#1478)
    #1470

--- packages/svelte2tsx/test/htmlx2jsx/samples/if-block-const/expected.jsx
+++ packages/svelte2tsx/test/htmlx2jsx/samples/if-block-const/expected.jsx
@@ -1,10 +1,28 @@
-<>{(name == "world") ? <>
-    {@const hello = name}
+<>{(name == "world") ? <>{(() => {const hello = name;return <>
+    { }
     <h1>Hello {hello}</h1>
-</> : (true) ? <>
-    {@const hello = name}
+</>})()}</> : (true) ? <>{(() => {const hello = name;return <>
+    { }
     <h1>Hello {hello}</h1>
-</> : <>
-    {@const hello = name}
+</>})()}</> : <>{(() => {const hello = name;return <>
+    { }
     <h1>Hello {hello}</h1>
+</>})()}</> }
+
+{(typeof a === 'string') ? <>{(() => {const aStr = a;const aStr2 = aStr;return <>
+    { }
+    { }
+
+    {a}
+</>})()}</> : (typeof a === 'number') ? <>{(() => {const aNum = a;return <>
+    { }
+</>})()}</> : <></> }
+
+{(typeof a === 'string') ? <>{(() => {const aStr = a;return <>
+    { }
+</>})()}</> : <></>}
+
+{(typeof a === 'string') ? <>{(() => {const aStr = a;return <>
+    { }
+</>})()}</> : <>
 </> }</>
