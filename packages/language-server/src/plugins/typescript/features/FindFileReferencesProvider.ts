6059620118339435212ea4a5e44cd8cf9eb80a1a
Jojoshua <jojoshua@gmail.com>
2022-05-23 23:24:50 -0400

    Fill in more of FindFileReferencesProviderImpl

--- packages/language-server/src/plugins/typescript/features/FindFileReferencesProvider.ts
+++ packages/language-server/src/plugins/typescript/features/FindFileReferencesProvider.ts
@@ -1,5 +1,4 @@
 import { Location } from 'vscode-languageserver';
-import { Document } from '../../../lib/documents';
 import { FileReferencesProvider } from '../../interfaces';
 import { LSAndTSDocResolver } from '../LSAndTSDocResolver';
 
@@ -7,6 +6,35 @@ export class FindFileReferencesProviderImpl implements FileReferencesProvider {
     constructor(private readonly lsAndTsDocResolver: LSAndTSDocResolver) {}
 
     async fileReferences(fileName: string): Promise<Location[] | null> {
-        //??
+        const lang = await this.getLSForPath(fileName);
+
+        const references = lang.getFileReferences(fileName);
+
+        if (!references) {
+            return null;
+        }
+
+        return [];
+
+        // const docs = new SnapshotFragmentMap(this.lsAndTsDocResolver);
+        // docs.set(tsDoc.filePath, { fragment, snapshot: tsDoc });
+
+        // const locations = await Promise.all(
+        //     references.map(async (ref) => {
+        //         const defDoc = await docs.retrieveFragment(ref.fileName);
+
+        //         return Location.create(
+        //             pathToUrl(ref.fileName),
+        //             convertToLocationRange(defDoc, ref.textSpan)
+        //         );
+        //     })
+        // );
+        // // Some references are in generated code but not wrapped with explicit ignore comments.
+        // // These show up as zero-length ranges, so filter them out.
+        // return locations.filter(hasNonZeroRange);
+    }
+
+    private async getLSForPath(path: string) {
+        return this.lsAndTsDocResolver.getLSForPath(path);
     }
 }
