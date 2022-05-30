0aded87a1f566c61490c71dd7e3d01b44581a08a
Jojoshua <jojoshua@gmail.com>
2022-05-29 21:55:20 -0400

    initial test

---
+++ packages/language-server/test/plugins/typescript/features/FindFileReferencesProvider.test.ts
@@ -0,0 +1,68 @@
+import * as assert from 'assert';
+import * as path from 'path';
+import ts from 'typescript';
+import { Location, Position, Range } from 'vscode-languageserver';
+import { Document, DocumentManager } from '../../../../src/lib/documents';
+import { LSConfigManager } from '../../../../src/ls-config';
+import { FindFileReferencesProviderImpl } from '../../../../src/plugins/typescript/features/FindFileReferencesProvider';
+import { LSAndTSDocResolver } from '../../../../src/plugins/typescript/LSAndTSDocResolver';
+import { pathToUrl } from '../../../../src/utils';
+
+const testDir = path.join(__dirname, '..');
+
+function test(useNewTransformation: boolean) {
+    return () => {
+        function getFullPath(filename: string) {
+            return path.join(testDir, 'testfiles', filename);
+        }
+        function getUri(filename: string) {
+            const filePath = path.join(testDir, 'testfiles', filename);
+            return pathToUrl(filePath);
+        }
+
+        function setup(filename: string) {
+            const docManager = new DocumentManager(
+                (textDocument) => new Document(textDocument.uri, textDocument.text)
+            );
+            const lsConfigManager = new LSConfigManager();
+            lsConfigManager.update({ svelte: { useNewTransformation } });
+            const lsAndTsDocResolver = new LSAndTSDocResolver(
+                docManager,
+                [testDir],
+                lsConfigManager
+            );
+            const provider = new FindFileReferencesProviderImpl(lsAndTsDocResolver);
+            const document = openDoc(filename);
+            return { provider, document };
+
+            function openDoc(filename: string) {
+                const filePath = getFullPath(filename);
+                const doc = docManager.openDocument(<any>{
+                    uri: pathToUrl(filePath),
+                    text: ts.sys.readFile(filePath) || ''
+                });
+                return doc;
+            }
+        }
+
+        async function test() {
+            const { provider, document } = setup('find-file-references-child.svelte');
+
+            const results = await provider.fileReferences(document.uri.toString());
+            const expectedResults = [
+                Location.create(
+                    getUri('find-file-references-parent.svelte'),
+                    Range.create(Position.create(1, 37), Position.create(1, 72))
+                )
+            ];
+
+            assert.deepStrictEqual(results, expectedResults);
+        }
+
+        it('finds file references', async () => {
+            await test();
+        });
+    };
+}
+
+describe('FindFileReferencesProvider', test(true));
---
+++ packages/language-server/test/plugins/typescript/testfiles/find-file-references-parent.svelte
@@ -0,0 +1,10 @@
+<script>
+import FindFileReferencesChild from "./find-file-references-child.svelte";
+
+    const findMe = true;
+    if (findMe) {
+        findMe;
+    }
+</script>
+
+<FindFileReferencesChild></FindFileReferencesChild>
