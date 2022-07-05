1fdb2b3e1cd53e44fb67d718097d07f9a8aba138
Simon <simon.holthausen@accso.de>
2022-07-05 12:55:35 +0200

    (fix) keep colon while transforming events
    on:click -> "on:click" instead of on:click -> "onclick"

--- packages/svelte2tsx/test/htmlx2jsx/samples/event-handler-quoted/expectedv2.js
+++ packages/svelte2tsx/test/htmlx2jsx/samples/event-handler-quoted/expectedv2.js
@@ -1,2 +1,2 @@
-  { svelteHTML.createElement("button", {  "onclick":(e) => {},}); }
-  { svelteHTML.createElement("button", {  "onclick":(e) => {},}); }
+  { svelteHTML.createElement("button", {  "on:click":(e) => {},}); }
+  { svelteHTML.createElement("button", {  "on:click":(e) => {},}); }
--- packages/svelte2tsx/test/svelte2tsx/samples/svelte-element/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/svelte-element/expectedv2.ts
@@ -9,7 +9,7 @@ async () => {
  { svelteHTML.createElement("tag", { });}
  { svelteHTML.createElement(tag ? 'a' : 'b', {  });}
  { svelteHTML.createElement(tag, { });tag; }
- { svelteHTML.createElement(tag, {    "onclick":() => tag,});}};
+ { svelteHTML.createElement(tag, {    "on:click":() => tag,});}};
 return { props: {}, slots: {}, getters: {}, events: {} }}
 
 export default class Input__SvelteComponent_ extends __sveltets_1_createSvelte2TsxComponent(__sveltets_1_partial(__sveltets_1_with_any_event(render()))) {
