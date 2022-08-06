e205e24e9134ace01b9a227aeb80c0a803e3646c
Pascal <pascal@pascal.com>
2022-08-06 19:18:43 -0400

    update samples for svelte-element

--- packages/svelte2tsx/test/svelte2tsx/samples/svelte-element/expected.tsx
+++ packages/svelte2tsx/test/svelte2tsx/samples/svelte-element/expected.tsx
@@ -9,7 +9,8 @@
 <svelteelement this="tag" />
 <svelteelement this={tag ? 'a' : 'b'} />
 <svelteelement this={tag}>{tag}</svelteelement>
-<svelteelement this={tag} onclick={() => tag} /></>);
+<svelteelement this={tag} onclick={() => tag} />
+<svelteelement this={'a'} sveltekitPrefetch href="https://kit.svelte.dev" /></>);
 return { props: {}, slots: {}, getters: {}, events: {} }}
 
 export default class Input__SvelteComponent_ extends __sveltets_1_createSvelte2TsxComponent(__sveltets_1_partial(__sveltets_1_with_any_event(render()))) {
