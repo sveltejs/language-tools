dce6c65d9bb6b46387981fc56f4faac62769f459
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-09-15 12:03:58 +0200

    fix uri creation, escape $types

--- packages/svelte-vscode/src/sveltekit/generateFiles/generate.ts
+++ packages/svelte-vscode/src/sveltekit/generateFiles/generate.ts
@@ -4,14 +4,13 @@ import { Position, Uri, window, workspace, WorkspaceEdit } from 'vscode';
 
 export async function generateResources(config: GenerateConfig) {
     workspace.fs.createDirectory(Uri.file(config.path));
-    const writtenFiles: string[] = [];
     const edit = new WorkspaceEdit();
 
     for (const resource of config.resources) {
         const ext = resource.type === FileType.PAGE ? config.pageExtension : config.scriptExtension;
         const filepath = join(config.path, `${resource.filename}.${ext}`);
 
-        const uri = Uri.parse(filepath);
+        const uri = Uri.file(filepath);
         edit.createFile(uri, {
             overwrite: false,
             ignoreIfExists: true
@@ -19,7 +18,6 @@ export async function generateResources(config: GenerateConfig) {
 
         const data = await resource.generate(config);
         edit.insert(uri, new Position(0, 0), data);
-        writtenFiles.push(filepath);
     }
 
     await workspace.applyEdit(edit);
