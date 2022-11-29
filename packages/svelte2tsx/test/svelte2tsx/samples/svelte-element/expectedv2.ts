25d0434f79b97d8a55e7dc6cb1e97fc4fd93ee8e
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-11-29 16:03:21 +0100

    fix

--- packages/svelte2tsx/test/svelte2tsx/samples/svelte-element/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/svelte-element/expectedv2.ts
@@ -13,6 +13,5 @@ async () => {
  { svelteHTML.createElement('a', {    "data-sveltekit-preload-data":true,"href":`https://kit.svelte.dev`,});}};
 return { props: /** @type {Record<string, never>} */ ({}), slots: {}, events: {} }}
 
-
 export default class Input__SvelteComponent_ extends __sveltets_1_createSvelte2TsxComponent(__sveltets_1_partial(__sveltets_1_with_any_event(render()))) {
 }
