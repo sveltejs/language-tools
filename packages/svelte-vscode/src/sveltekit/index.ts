e23d9d7dee806c3775bac7547f9b5728008b8ac9
Lyu, Wei Da <b00605009@ntu.edu.tw>
2022-12-10 11:20:14 +0800

    (feat) only show SvelteKit files context menu in kit project

--- packages/svelte-vscode/src/sveltekit/index.ts
+++ packages/svelte-vscode/src/sveltekit/index.ts
@@ -1,6 +1,71 @@
-import { ExtensionContext } from 'vscode';
+import { TextDecoder } from 'util';
+import { ExtensionContext, commands, workspace } from 'vscode';
 import { addGenerateKitRouteFilesCommand } from './generateFiles';
 
+type ShowSvelteKitFilesCommandConfig = 'auto' | 'on' | 'off';
+
 export function setupSvelteKit(context: ExtensionContext) {
+    let contextMenuEnabled = false;
+    context.subscriptions.push(
+        workspace.onDidChangeConfiguration(() => {
+            enableContextMenu();
+        })
+    );
+
     addGenerateKitRouteFilesCommand(context);
+    enableContextMenu();
+
+    async function enableContextMenu() {
+        const config = getConfig();
+        if (config === 'off') {
+            if (contextMenuEnabled) {
+                setEnableContext(false);
+            }
+            return;
+        }
+
+        if (config === 'on') {
+            // force on
+            return;
+        }
+
+        if (await detect()) {
+            setEnableContext(true);
+            contextMenuEnabled = true;
+        }
+    }
+}
+
+function getConfig() {
+    return (
+        workspace
+            .getConfiguration('svelte.ui')
+            .get<ShowSvelteKitFilesCommandConfig>('showSvelteKitFilesCommand') ?? 'auto'
+    );
+}
+
+async function detect() {
+    const packageJsonList = await workspace.findFiles('**/package.json', 'node_modules');
+
+    for (const fileUri of packageJsonList) {
+        try {
+            const text = new TextDecoder().decode(await workspace.fs.readFile(fileUri));
+            const pkg = JSON.parse(text);
+            const hasKit = Object.keys(pkg.devDependencies ?? {})
+                .concat(Object.keys(pkg.dependencies ?? {}))
+                .includes('@sveltejs/kit');
+
+            if (hasKit) {
+                return true;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return false;
+}
+
+function setEnableContext(enable: boolean) {
+    commands.executeCommand('setContext', 'svelte.uiContext.showSvelteKitFilesCommand', enable);
 }
