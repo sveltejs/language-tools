08639cf2a6c4be45daf0431189ded5076cef5e95
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-12-20 13:23:41 +0100

    (fix) move interface if referenced by generic (#1791)
    ...out of the render function, else it's used before being defined
    #1335

--- packages/svelte2tsx/src/svelte2tsx/nodes/handleImportDeclaration.ts
+++ packages/svelte2tsx/src/svelte2tsx/nodes/handleImportDeclaration.ts
@@ -1,5 +1,6 @@
 import MagicString from 'magic-string';
 import ts from 'typescript';
+import { moveNode } from '../utils/tsAst';
 
 /**
  * move imports to top of script so they appear outside our render function
@@ -11,62 +12,7 @@ export function handleImportDeclaration(
     scriptStart: number,
     sourceFile: ts.SourceFile
 ) {
-    const scanner = ts.createScanner(
-        sourceFile.languageVersion,
-        /*skipTrivia*/ false,
-        sourceFile.languageVariant
-    );
-
-    const comments = ts.getLeadingCommentRanges(node.getFullText(), 0) ?? [];
-    if (
-        !comments.some((comment) => comment.hasTrailingNewLine) &&
-        isNewGroup(sourceFile, node, scanner)
-    ) {
-        str.appendRight(node.getStart() + astOffset, '\n');
-    }
-
-    for (const comment of comments) {
-        const commentEnd = node.pos + comment.end + astOffset;
-        str.move(node.pos + comment.pos + astOffset, commentEnd, scriptStart + 1);
-
-        if (comment.hasTrailingNewLine) {
-            str.overwrite(commentEnd - 1, commentEnd, str.original[commentEnd - 1] + '\n');
-        }
-    }
-
-    str.move(node.getStart() + astOffset, node.end + astOffset, scriptStart + 1);
-    //add in a \n
-    const originalEndChar = str.original[node.end + astOffset - 1];
-
-    str.overwrite(node.end + astOffset - 1, node.end + astOffset, originalEndChar + '\n');
-}
-
-/**
- * adopted from https://github.com/microsoft/TypeScript/blob/6e0447fdf165b1cec9fc80802abcc15bd23a268f/src/services/organizeImports.ts#L111
- */
-function isNewGroup(
-    sourceFile: ts.SourceFile,
-    topLevelImportDecl: ts.ImportDeclaration,
-    scanner: ts.Scanner
-) {
-    const startPos = topLevelImportDecl.getFullStart();
-    const endPos = topLevelImportDecl.getStart();
-    scanner.setText(sourceFile.text, startPos, endPos - startPos);
-
-    let numberOfNewLines = 0;
-    while (scanner.getTokenPos() < endPos) {
-        const tokenKind = scanner.scan();
-
-        if (tokenKind === ts.SyntaxKind.NewLineTrivia) {
-            numberOfNewLines++;
-
-            if (numberOfNewLines >= 2) {
-                return true;
-            }
-        }
-    }
-
-    return false;
+    return moveNode(node, str, astOffset, scriptStart, sourceFile);
 }
 
 /**
--- packages/svelte2tsx/src/svelte2tsx/processInstanceScriptContent.ts
+++ packages/svelte2tsx/src/svelte2tsx/processInstanceScriptContent.ts
@@ -1,7 +1,7 @@
 import MagicString from 'magic-string';
 import { Node } from 'estree-walker';
 import * as ts from 'typescript';
-import { getBinaryAssignmentExpr, isNotPropertyNameOfImport } from './utils/tsAst';
+import { getBinaryAssignmentExpr, isNotPropertyNameOfImport, moveNode } from './utils/tsAst';
 import { ExportedNames, is$$PropsDeclaration } from './nodes/ExportedNames';
 import { ImplicitTopLevelNames } from './nodes/ImplicitTopLevelNames';
 import { ComponentEvents, is$$EventsDeclaration } from './nodes/ComponentEvents';
@@ -15,6 +15,7 @@ import {
     handleFirstInstanceImport,
     handleImportDeclaration
 } from './nodes/handleImportDeclaration';
+import { InterfacesAndTypes } from './nodes/InterfacesAndTypes';
 
 export interface InstanceScriptProcessResult {
     exportedNames: ExportedNames;
@@ -52,6 +53,7 @@ export function processInstanceScriptContent(
     const astOffset = script.content.start;
     const exportedNames = new ExportedNames(str, astOffset);
     const generics = new Generics(str, astOffset);
+    const interfacesAndTypes = new InterfacesAndTypes();
 
     const implicitTopLevelNames = new ImplicitTopLevelNames(str, astOffset);
     let uses$$props = false;
@@ -219,6 +221,12 @@ export function processInstanceScriptContent(
             implicitStoreValues.addImportStatement(node);
         }
 
+        if (ts.isTypeAliasDeclaration(node) || ts.isInterfaceDeclaration(node)) {
+            interfacesAndTypes.node = node;
+            interfacesAndTypes.add(node);
+            onLeaveCallbacks.push(() => (interfacesAndTypes.node = null));
+        }
+
         //handle stores etc
         if (ts.isIdentifier(node)) {
             handleIdentifier(node, parent);
@@ -264,12 +272,19 @@ export function processInstanceScriptContent(
 
     handleFirstInstanceImport(tsAst, astOffset, hasModuleScript, str);
 
+    // move interfaces and types out of the render function if they are referenced
+    // by a $$Generic, otherwise it will be used before being defined after the transformation
+    const nodesToMove = interfacesAndTypes.getNodesWithNames(generics.getTypeReferences());
+    for (const node of nodesToMove) {
+        moveNode(node, str, astOffset, script.start, tsAst);
+    }
+
     if (mode === 'dts') {
         // Transform interface declarations to type declarations because indirectly
         // using interfaces inside the return type of a function is forbidden.
         // This is not a problem for intellisense/type inference but it will
         // break dts generation (file will not be generated).
-        transformInterfacesToTypes(tsAst, str, astOffset);
+        transformInterfacesToTypes(tsAst, str, astOffset, nodesToMove);
     }
 
     return {
@@ -283,32 +298,40 @@ export function processInstanceScriptContent(
     };
 }
 
-function transformInterfacesToTypes(tsAst: ts.SourceFile, str: MagicString, astOffset: any) {
-    tsAst.statements.filter(ts.isInterfaceDeclaration).forEach((node) => {
-        str.overwrite(
-            node.getStart() + astOffset,
-            node.getStart() + astOffset + 'interface'.length,
-            'type'
-        );
+function transformInterfacesToTypes(
+    tsAst: ts.SourceFile,
+    str: MagicString,
+    astOffset: any,
+    movedNodes: ts.Node[]
+) {
+    tsAst.statements
+        .filter(ts.isInterfaceDeclaration)
+        .filter((i) => !movedNodes.includes(i))
+        .forEach((node) => {
+            str.overwrite(
+                node.getStart() + astOffset,
+                node.getStart() + astOffset + 'interface'.length,
+                'type'
+            );
 
-        if (node.heritageClauses?.length) {
-            const extendsStart = node.heritageClauses[0].getStart() + astOffset;
-            str.overwrite(extendsStart, extendsStart + 'extends'.length, '=');
+            if (node.heritageClauses?.length) {
+                const extendsStart = node.heritageClauses[0].getStart() + astOffset;
+                str.overwrite(extendsStart, extendsStart + 'extends'.length, '=');
 
-            const extendsList = node.heritageClauses[0].types;
-            let prev = extendsList[0];
-            extendsList.slice(1).forEach((heritageClause) => {
-                str.overwrite(
-                    prev.getEnd() + astOffset,
-                    heritageClause.getStart() + astOffset,
-                    ' & '
-                );
-                prev = heritageClause;
-            });
+                const extendsList = node.heritageClauses[0].types;
+                let prev = extendsList[0];
+                extendsList.slice(1).forEach((heritageClause) => {
+                    str.overwrite(
+                        prev.getEnd() + astOffset,
+                        heritageClause.getStart() + astOffset,
+                        ' & '
+                    );
+                    prev = heritageClause;
+                });
 
-            str.appendLeft(node.heritageClauses[0].getEnd() + astOffset, ' & ');
-        } else {
-            str.prependLeft(str.original.indexOf('{', node.getStart() + astOffset), '=');
-        }
-    });
+                str.appendLeft(node.heritageClauses[0].getEnd() + astOffset, ' & ');
+            } else {
+                str.prependLeft(str.original.indexOf('{', node.getStart() + astOffset), '=');
+            }
+        });
 }
