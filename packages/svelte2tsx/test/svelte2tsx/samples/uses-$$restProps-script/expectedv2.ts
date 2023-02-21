3c871e1d1ea74d4214e112e0ae0bb8e617e14b54
Simon Holthausen <simon.holthausen@vercel.com>
2023-02-21 11:08:34 +0100

    (fix) no error with unkown props if uses $$restProps
    honor $$restProps when creating props type

--- packages/svelte2tsx/test/svelte2tsx/samples/uses-$$restProps-script/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/uses-$$restProps-script/expectedv2.ts
@@ -5,7 +5,7 @@
 ;
 async () => { { svelteHTML.createElement("h1", {});name; }
 };
-return { props: /** @type {Record<string, never>} */ ({}), slots: {}, events: {} }}
+return { props: {}, slots: {}, events: {} }}
 
 export default class Input__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_partial_with_any(__sveltets_2_with_any_event(render()))) {
 }
