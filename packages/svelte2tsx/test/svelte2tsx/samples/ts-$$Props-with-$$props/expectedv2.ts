5dfeef27bbeb3c5652a754cbf475840b027bbab5
Simon Holthausen <simon.holthausen@vercel.com>
2023-03-08 15:15:49 +0100

    (fix) remove overzealous $$Props check
    The check if exports are assignable to $$Props is removed because a component should be allowed to use less props than defined (it just ignores them)

--- packages/svelte2tsx/test/svelte2tsx/samples/ts-$$Props-with-$$props/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-$$Props-with-$$props/expectedv2.ts
@@ -13,7 +13,7 @@
 async () => {
 
 $$props;};
-return { props: {...__sveltets_2_ensureRightProps<{}>(__sveltets_2_any("") as $$Props), ...__sveltets_2_ensureRightProps<Partial<$$Props>>({}), ...{} as unknown as $$Props, ...{c: c} as {c?: typeof c}}, slots: {}, events: {} }}
+return { props: {...__sveltets_2_ensureRightProps<{}>(__sveltets_2_any("") as $$Props), ...{} as unknown as $$Props, ...{c: c} as {c?: typeof c}}, slots: {}, events: {} }}
 
 export default class Input__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_with_any_event(render())) {
     get c() { return __sveltets_2_nonNullable(this.$$prop_def.c) }
