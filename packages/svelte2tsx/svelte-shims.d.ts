8de261f1a80009e1a1d00664d3f0a4c27784e7e2
Simon H <5968653+dummdidumm@users.noreply.github.com>
2023-03-10 15:30:06 +0100

    (fix) allow more actions on one element
    up to 10 now
    #1907

--- packages/svelte2tsx/svelte-shims.d.ts
+++ packages/svelte2tsx/svelte-shims.d.ts
@@ -218,7 +218,7 @@ declare function __sveltets_2_createComponentAny(props: Record<string, any>): im
 
 declare function __sveltets_2_any(...dummy: any[]): any;
 declare function __sveltets_2_empty(...dummy: any[]): {};
-declare function __sveltets_2_union<T1,T2,T3,T4,T5>(t1:T1,t2?:T2,t3?:T3,t4?:T4,t5?:T5): T1 & T2 & T3 & T4 & T5;
+declare function __sveltets_2_union<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10>(t1:T1,t2?:T2,t3?:T3,t4?:T4,t5?:T5,t6?:T6,t7?:T7,t8?:T8,t9?:T9,t10?:T10): T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10;
 declare function __sveltets_2_nonNullable<T>(type: T): NonNullable<T>;
 
 declare function __sveltets_2_cssProp(prop: Record<string, any>): {};
