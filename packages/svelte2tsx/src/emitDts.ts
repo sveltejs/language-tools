5f9ce916155f2d9b2a962f98ebd75a25e8c43ea6
Simon H <5968653+dummdidumm@users.noreply.github.com>
2023-03-15 12:26:32 +0100

    (chore) mark svelte2tsx as compatible with TS 5 (#1940)
    also add resolveModuleNameLiterals to emitDts

--- packages/svelte2tsx/src/emitDts.ts
+++ packages/svelte2tsx/src/emitDts.ts
@@ -176,6 +176,23 @@ async function createTsCompilerHost(options: any, svelteMap: SvelteMap) {
             return resolveModuleName(moduleName, containingFile, compilerOptions);
         });
     };
+    // @ts-expect-error remove once we bump dev dep to TS 5
+    host.resolveModuleNameLiterals = (
+        moduleLiterals,
+        containingFile,
+        _redirectedReference,
+        compilerOptions
+    ) => {
+        return moduleLiterals.map((moduleLiteral) => {
+            return {
+                resolvedModule: resolveModuleName(
+                    moduleLiteral.text,
+                    containingFile,
+                    compilerOptions
+                )
+            };
+        });
+    };
 
     function resolveModuleName(name: string, containingFile: string, compilerOptions: any) {
         // Delegate to the TS resolver first.
