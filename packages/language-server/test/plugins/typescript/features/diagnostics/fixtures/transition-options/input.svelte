13a8b195627344307089589e079976a0d8d4e85b
Lyu Jason <b00605009@ntu.edu.tw>
2023-08-26 12:07:02 +0800

    (fix) ignore error about transition third argument

---
+++ packages/language-server/test/plugins/typescript/features/diagnostics/fixtures/transition-options/input.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    function myTransition(
+      _node: HTMLElement,
+      _params: { delay: number },
+      _context: { direction: 'in' | 'out' | 'both' }
+    ) {
+      return {};
+    }
+</script>
+
+<div in:myTransition={{ delay: 100 }} />
