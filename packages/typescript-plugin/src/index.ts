df82ee3029d1697382ce0c4b321f28bf5f7d111e
Johnson Chu <johnsoncodehk@gmail.com>
2023-12-27 17:42:51 +0800

    `module.exports = ` -> `export  = `

--- packages/typescript-plugin/src/index.ts
+++ packages/typescript-plugin/src/index.ts
@@ -1,7 +1,7 @@
 import { createTSServerPlugin } from '@volar/typescript/lib/quickstart/createTSServerPlugin';
 import { svelteLanguagePlugin } from 'svelte-language-server/out/languagePlugin';
 
-module.exports = createTSServerPlugin(
+export = createTSServerPlugin(
   () => {
     return {
       languagePlugins: [svelteLanguagePlugin],
