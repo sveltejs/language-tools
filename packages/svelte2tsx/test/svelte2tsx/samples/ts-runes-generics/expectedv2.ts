f5ec504fb7a2108b33ff16b628eff95f954e03cb
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-01-11 15:37:48 +0100

    feat: support rename/find reference for `$props()` (#2255)
    Generate a virtual type that is reused for the type/generics and the generated props type which makes renaming/find references etc "just work" without additional code in the language server
    #2253

--- packages/svelte2tsx/test/svelte2tsx/samples/ts-runes-generics/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-runes-generics/expectedv2.ts
@@ -1,7 +1,7 @@
 ///<reference types="svelte" />
 ;function render() {
-
-    let { a, b } = $props<{ a: number, b: string }>();
+;type $$_sveltets_Props = { a: number, b: string };
+    let { a, b } = $props<$$_sveltets_Props>();
     let x = $state(0);
     let y = $derived(x * 2);
 
@@ -9,7 +9,7 @@
 async () => {
 
  { __sveltets_createSlot("default", {  x,y,});}};
-return { props: {} as any as { a: number, b: string }, slots: {'default': {x:x, y:y}}, events: {} }}
+return { props: {} as any as $$_sveltets_Props, slots: {'default': {x:x, y:y}}, events: {} }}
 
 export default class Input__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_with_any_event(render())) {
 }
