b3541413fb8eb397903ca32c4e2bd92e635d00e9
Simon Holthausen <simon.holthausen@vercel.com>
2024-02-07 14:54:17 +0100

    fix: implict children tweaks
    - ignore comments when determining whether or not to add an implicit children prop #2263
    - do static analysis of the destructuring to see whether or not children is destructured from `$props()`, and if not, add the implicit children prop if there's a slot. This results in less confusing type errors when transforming to runes mode and using a slot

--- packages/svelte2tsx/test/svelte2tsx/samples/runes/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/runes/expectedv2.ts
@@ -1,16 +1,13 @@
 ///<reference types="svelte" />
 ;function render() {
 
-    /** @type {a: number, b: string} */
+    /** @typedef {{a: number, b: string}}  $$_sveltets_Props *//** @type {$$_sveltets_Props} */
     let { a, b } = $props();
     let x = $state(0);
     let y = $derived(x * 2);
-
-/*Ωignore_startΩ*/;const __sveltets_createSlot = __sveltets_2_createCreateSlot();/*Ωignore_endΩ*/;
-async () => {
-
- { __sveltets_createSlot("default", {  x,y,});}};
-return { props: /** @type {a: number, b: string} */({}), slots: {'default': {x:x, y:y}}, events: {} }}
+;
+async () => {};
+return { props: /** @type {$$_sveltets_Props} */({}), slots: {}, events: {} }}
 
 export default class Input__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_partial(__sveltets_2_with_any_event(render()))) {
 }
--- packages/svelte2tsx/test/svelte2tsx/samples/sveltekit-autotypes-$props-rune/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/sveltekit-autotypes-$props-rune/expectedv2.ts
@@ -5,7 +5,7 @@
      const snapshot/*Ωignore_startΩ*/: import('./$types.js').Snapshot/*Ωignore_endΩ*/ = {};
 ;
 async () => {};
-return { props: /** @type {{ data: import('./$types.js').PageData, form: import('./$types.js').ActionData }} */({}), slots: {}, events: {} }}
+return { props: /** @type {{snapshot?: typeof snapshot} & { data: import('./$types.js').PageData, form: import('./$types.js').ActionData }} */({}), slots: {}, events: {} }}
 
 export default class Page__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_partial(['snapshot'], __sveltets_2_with_any_event(render()))) {
     get snapshot() { return __sveltets_2_nonNullable(this.$$prop_def.snapshot) }
