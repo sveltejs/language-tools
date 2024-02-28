ff2f10a1a80b146df51374060358dd34e63432df
Simon Holthausen <simon.holthausen@vercel.com>
2024-02-28 11:20:45 +0100

    chore: update tests

--- packages/svelte2tsx/test/svelte2tsx/samples/ts-sveltekit-autotypes-$props-rune/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-sveltekit-autotypes-$props-rune/expectedv2.ts
@@ -1,11 +1,11 @@
 ///<reference types="svelte" />
 ;function render() {
 
-     const snapshot/*Ωignore_startΩ*/: import('./$types.js').Snapshot/*Ωignore_endΩ*/ = {};
-    let { form, data } = $props/*Ωignore_startΩ*/<{ data: import('./$types.js').PageData, form: import('./$types.js').ActionData }>/*Ωignore_endΩ*/();
+     const snapshot/*Ωignore_startΩ*/: import('./$types.js').Snapshot/*Ωignore_endΩ*/ = {};/*Ωignore_startΩ*/;type $$_sveltets_Props = { form: import('./$types.js').ActionData, data: import('./$types.js').PageData };/*Ωignore_endΩ*/
+    let { form, data } = $props<$$_sveltets_Props>();
 ;
 async () => {};
-return { props: {} as any as { data: import('./$types.js').PageData, form: import('./$types.js').ActionData } & { snapshot?: typeof snapshot }, slots: {}, events: {} }}
+return { props: {} as any as $$_sveltets_Props & { snapshot?: typeof snapshot }, slots: {}, events: {} }}
 
 export default class Page__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_with_any_event(render())) {
     get snapshot() { return __sveltets_2_nonNullable(this.$$prop_def.snapshot) }
