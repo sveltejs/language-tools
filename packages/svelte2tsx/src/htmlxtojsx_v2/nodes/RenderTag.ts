c27a99b6570f91781e7321ee5d4c248bf535d9bd
Simon Holthausen <simon.holthausen@vercel.com>
2024-02-28 10:30:00 +0100

    fix: adjust render tag for latest AST version

--- packages/svelte2tsx/src/htmlxtojsx_v2/nodes/RenderTag.ts
+++ packages/svelte2tsx/src/htmlxtojsx_v2/nodes/RenderTag.ts
@@ -10,16 +10,23 @@ export function handleRenderTag(str: MagicString, renderTag: BaseNode): void {
         contentOnly: true
     });
 
-    // argument was present until https://github.com/sveltejs/svelte/pull/9988, remove at some point
+    // argument was present until https://github.com/sveltejs/svelte/pull/9988 / https://github.com/sveltejs/svelte/pull/10656,
+    // remove and only keep last else block at some point
     const arg = renderTag.argument || renderTag.arguments?.[renderTag.arguments.length - 1];
 
     if (arg) {
         str.overwrite(withTrailingPropertyAccess(str.original, arg.end), renderTag.end, '));');
-    } else {
+    } else if ('argument' in renderTag || 'arguments' in renderTag) {
         str.overwrite(
             withTrailingPropertyAccess(str.original, renderTag.expression.end),
             renderTag.end,
             '());'
+        );
+    } else {
+        str.overwrite(
+            withTrailingPropertyAccess(str.original, renderTag.expression.end),
+            renderTag.end,
+            ');'
         );
     }
 }
