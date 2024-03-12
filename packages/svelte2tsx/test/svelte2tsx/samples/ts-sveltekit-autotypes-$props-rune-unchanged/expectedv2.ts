57b64296b095e115c3b778f7580345c478cf0178
Simon Holthausen <simon.holthausen@vercel.com>
2024-03-12 15:29:18 +0100

    fix: use type instead of generic for `$props()`
    generic argument will eventually be removed
    also fixes TS to 5.3 until we resolve the issues with 5.4

--- packages/svelte2tsx/test/svelte2tsx/samples/ts-sveltekit-autotypes-$props-rune-unchanged/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-sveltekit-autotypes-$props-rune-unchanged/expectedv2.ts
@@ -1,11 +1,11 @@
 ///<reference types="svelte" />
 ;function render() {
 
-     const snapshot: any = {};;type $$_sveltets_Props = {form: boolean, data: true };
-    let { form, data } = $props<$$_sveltets_Props>();
+     const snapshot: any = {};;type $$ComponentProps =  {form: boolean, data: true };
+    let { form, data }:$$ComponentProps = $props();
 ;
 async () => {};
-return { props: {} as any as $$_sveltets_Props & { snapshot?: any }, slots: {}, events: {} }}
+return { props: {} as any as $$ComponentProps & { snapshot?: any }, slots: {}, events: {} }}
 
 export default class Page__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_with_any_event(render())) {
     get snapshot() { return __sveltets_2_nonNullable(this.$$prop_def.snapshot) }
