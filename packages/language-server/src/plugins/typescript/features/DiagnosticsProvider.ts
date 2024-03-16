35510ed4a0cdc33cdc51512088cfb1b33f4ec64d
Lyu Jason <b00605009@ntu.edu.tw>
2024-03-16 12:44:21 +0800

    oops this doesn't work in 5.3

--- packages/language-server/src/plugins/typescript/features/DiagnosticsProvider.ts
+++ packages/language-server/src/plugins/typescript/features/DiagnosticsProvider.ts
@@ -545,13 +545,14 @@ function expectedTransitionThirdArgument(
 
     // in TypeScript 5.4 the error is on the function name
     // in earlier versions it's on the whole call expression
-    const callExpression = ts.isCallExpression(node.parent)
-        ? node.parent
-        : findNodeAtSpan(
-              node,
-              { start: node.getStart(), length: node.getWidth() },
-              ts.isCallExpression
-          );
+    const callExpression =
+        ts.isIdentifier(node) && ts.isCallExpression(node.parent)
+            ? node.parent
+            : findNodeAtSpan(
+                  node,
+                  { start: node.getStart(), length: node.getWidth() },
+                  ts.isCallExpression
+              );
 
     const signature =
         callExpression && lang.getProgram()?.getTypeChecker().getResolvedSignature(callExpression);
