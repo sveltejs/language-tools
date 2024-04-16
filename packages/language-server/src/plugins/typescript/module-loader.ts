4c8c0db10455367c486a3e03555da5290050bf48
Lyu, Wei-Da <36730922+jasonlyu123@users.noreply.github.com>
2024-04-16 23:42:17 +0800

    feat: editor support for ts 5.4 (#2334)
    Follow up to #2313. The auto-import problem is fixed in 5.4.5.

--- packages/language-server/src/plugins/typescript/module-loader.ts
+++ packages/language-server/src/plugins/typescript/module-loader.ts
@@ -107,7 +107,6 @@ class ImpliedNodeFormatResolver {
         let mode = undefined;
         if (sourceFile) {
             this.cacheImpliedNodeFormat(sourceFile, compilerOptions);
-            // @ts-expect-error remove when bumping to TS 5.4
             mode = ts.getModeForResolutionAtIndex(sourceFile, importIdxInFile, compilerOptions);
         }
         return mode;
