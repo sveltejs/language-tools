6aa40f78bec15c1f0bff3bbf6722bdac10224262
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-04-25 09:34:55 +0200

    fix: handle `#await` inside `#snippet` (#2348)
    #2342
    also removes old versions of snippet and render tag AST

--- packages/svelte2tsx/src/htmlxtojsx_v2/nodes/RenderTag.ts
+++ packages/svelte2tsx/src/htmlxtojsx_v2/nodes/RenderTag.ts
@@ -9,24 +9,9 @@ export function handleRenderTag(str: MagicString, renderTag: BaseNode): void {
     str.overwrite(renderTag.start, renderTag.expression.start, ';__sveltets_2_ensureSnippet(', {
         contentOnly: true
     });
-
-    // argument was present until https://github.com/sveltejs/svelte/pull/9988 / https://github.com/sveltejs/svelte/pull/10656,
-    // remove and only keep last else block at some point
-    const arg = renderTag.argument || renderTag.arguments?.[renderTag.arguments.length - 1];
-
-    if (arg) {
-        str.overwrite(withTrailingPropertyAccess(str.original, arg.end), renderTag.end, '));');
-    } else if ('argument' in renderTag || 'arguments' in renderTag) {
-        str.overwrite(
-            withTrailingPropertyAccess(str.original, renderTag.expression.end),
-            renderTag.end,
-            '());'
-        );
-    } else {
-        str.overwrite(
-            withTrailingPropertyAccess(str.original, renderTag.expression.end),
-            renderTag.end,
-            ');'
-        );
-    }
+    str.overwrite(
+        withTrailingPropertyAccess(str.original, renderTag.expression.end),
+        renderTag.end,
+        ');'
+    );
 }
