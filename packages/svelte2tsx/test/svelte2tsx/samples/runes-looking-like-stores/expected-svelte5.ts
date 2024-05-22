1cf5d376dc36d47495c56a106ebc9e7f14f3df71
Simon Holthausen <simon.holthausen@vercel.com>
2024-05-22 15:11:05 +0200

    tests

--- packages/svelte2tsx/test/svelte2tsx/samples/runes-looking-like-stores/expected-svelte5.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/runes-looking-like-stores/expected-svelte5.ts
@@ -1,13 +1,14 @@
 ///<reference types="svelte" />
 ;function render() {
 
-    let x = $state();
-     function foo() { return true; }
+    let/** @typedef {{ props: unknown }} $$ComponentProps *//** @type {$$ComponentProps} */ { props } = $props();
+    let state = $state(0);
+    let derived = $derived(state * 2);
 ;
 async () => {
 
-x;};
-return { props: /** @type {Record<string, never>} */ ({}), exports: /** @type {{foo: typeof foo}} */ ({}), bindings: __sveltets_$$bindings(''), slots: {}, events: {} }}
+state; derived;};
+return { props: /** @type {$$ComponentProps} */({}), bindings: __sveltets_$$bindings(''), slots: {}, events: {} }}
 const Input__SvelteComponent_ = __sveltets_2_isomorphic_component(__sveltets_2_with_any_event(render()));
 /*Ωignore_startΩ*/type Input__SvelteComponent_ = InstanceType<typeof Input__SvelteComponent_>;
 /*Ωignore_endΩ*/export default Input__SvelteComponent_;
--- packages/svelte2tsx/test/svelte2tsx/samples/ts-runes/expected-svelte5.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-runes/expected-svelte5.ts
@@ -1,13 +1,12 @@
 ///<reference types="svelte" />
 ;function render() {
-
-    let x = $state();
-     function foo() { return true; }
+;type $$ComponentProps =  { a: number, b: string };
+    let { a, b }:/*Ωignore_startΩ*/$$ComponentProps/*Ωignore_endΩ*/ = $props();
+    let x = $state(0);
+    let y = $derived(x * 2);
 ;
-async () => {
-
-x;};
-return { props: /** @type {Record<string, never>} */ ({}), exports: /** @type {{foo: typeof foo}} */ ({}), bindings: __sveltets_$$bindings(''), slots: {}, events: {} }}
+async () => {};
+return { props: {} as any as $$ComponentProps, bindings: __sveltets_$$bindings(''), slots: {}, events: {} }}
 const Input__SvelteComponent_ = __sveltets_2_isomorphic_component(__sveltets_2_with_any_event(render()));
 /*Ωignore_startΩ*/type Input__SvelteComponent_ = InstanceType<typeof Input__SvelteComponent_>;
 /*Ωignore_endΩ*/export default Input__SvelteComponent_;
