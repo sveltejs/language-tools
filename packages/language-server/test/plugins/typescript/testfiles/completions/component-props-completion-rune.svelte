36981e70aaee7baf960d87e64c1f08983fd80b7e
Lyu, Wei-Da <36730922+jasonlyu123@users.noreply.github.com>
2024-06-19 23:44:04 +0800

    fix: support more types of component type defintion in completion (#2407)
    * fix: support more types of component type definition
    
    * ts-ignore Component import
    
    * format

---
+++ packages/language-server/test/plugins/typescript/testfiles/completions/component-props-completion-rune.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import ComponentPropsRune from './component-props-rune.svelte'
+    import { ComponentDef5 } from './ComponentDef'
+</script>
+
+<ComponentPropsRune />
+<ComponentDef5 />
