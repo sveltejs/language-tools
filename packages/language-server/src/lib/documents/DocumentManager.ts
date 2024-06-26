f9269bf7b8e737ea9005fcf9ccc5bb5fa6f4c0ea
Lyu, Wei Da <b00605009@ntu.edu.tw>
2024-06-26 15:30:43 +0800

    revert

--- packages/language-server/src/lib/documents/DocumentManager.ts
+++ packages/language-server/src/lib/documents/DocumentManager.ts
@@ -47,10 +47,7 @@ export class DocumentManager {
         let document: Document;
         if (this.documents.has(textDocument.uri)) {
             document = this.documents.get(textDocument.uri)!;
-            // open state should only be update when the document is closed            
-            if (document.openedByClient && !openedByClient) {
-                console.trace('why is this happening?')
-            }
+            // open state should only be update when the document is closed
             document.openedByClient ||= openedByClient;
             document.setText(textDocument.text);
         } else {
