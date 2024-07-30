719f17be66e1a1b0f0eedac70c65a5759e2c6577
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-07-30 18:22:57 +0200

    fix: add legacy methods to own component shape (#2451)
    see https://github.com/sveltejs/svelte/pull/12666 for why we need this

--- packages/svelte2tsx/src/svelte2tsx/addComponentExport.ts
+++ packages/svelte2tsx/src/svelte2tsx/addComponentExport.ts
@@ -167,7 +167,7 @@ function addSimpleComponentExport({
             statement =
                 `\ninterface $$__sveltets_2_IsomorphicComponent<Props extends Record<string, any> = any, Events extends Record<string, any> = any, Slots extends Record<string, any> = any, Exports = {}, Bindings = string> {
     new (options: import('svelte').ComponentConstructorOptions<Props>): import('svelte').SvelteComponent<Props, Events, Slots> & { $$bindings?: Bindings } & Exports;
-    (internal: unknown, props: ${!canHaveAnyProp && exportedNames.hasNoProps() ? '{$$events?: Events, $$slots?: Slots}' : 'Props & {$$events?: Events, $$slots?: Slots}'}): Exports;
+    (internal: unknown, props: ${!canHaveAnyProp && exportedNames.hasNoProps() ? '{$$events?: Events, $$slots?: Slots}' : 'Props & {$$events?: Events, $$slots?: Slots}'}): Exports & { $set?: any, $on?: any };
     z_$$bindings?: Bindings;
 }\n` +
                 (usesSlots
--- packages/svelte2tsx/test/svelte2tsx/samples/creates-no-script-dts/expected-svelte5.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/creates-no-script-dts/expected-svelte5.ts
@@ -8,7 +8,7 @@ async () => { { svelteHTML.createElement("button", { "on:click":undefined,}); { 
 return { props: /** @type {Record<string, never>} */ ({}), exports: {}, bindings: "", slots: {'default': {}}, events: {'click':__sveltets_2_mapElementEvent('click')} }}
 interface $$__sveltets_2_IsomorphicComponent<Props extends Record<string, any> = any, Events extends Record<string, any> = any, Slots extends Record<string, any> = any, Exports = {}, Bindings = string> {
     new (options: import('svelte').ComponentConstructorOptions<Props>): import('svelte').SvelteComponent<Props, Events, Slots> & { $$bindings?: Bindings } & Exports;
-    (internal: unknown, props: {$$events?: Events, $$slots?: Slots}): Exports;
+    (internal: unknown, props: {$$events?: Events, $$slots?: Slots}): Exports & { $set?: any, $on?: any };
     z_$$bindings?: Bindings;
 }
 type $$__sveltets_2_PropsWithChildren<Props, Slots> = Props &
