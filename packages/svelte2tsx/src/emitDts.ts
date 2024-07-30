sveltejs/language-tools

commit 527c2adb2fc6f13674bcc73cf52b63370dc0c8db

fix: correctly detect real Svelte files for declarationMap (#2443)
#2442

packages/svelte2tsx/src/emitDts.ts
@@ -172,10 +172,6 @@ async function createTsCompilerHost(options: any, svelteMap: SvelteMap) {
             fileName = pathPrefix ? path.join(pathPrefix, fileName) : fileName;
             if (fileName.endsWith('d.ts.map')) {
                 data = data.replace(/"sources":\["(.+?)"\]/, (_, sourcePath: string) => {
-                    // Due to our hack of treating .svelte files as .ts files, we need to adjust the extension
-                    if (sourcePath.endsWith('.svelte.ts')) {
-                        sourcePath = sourcePath.slice(0, -3);
-                    }
                     // The inverse of the pathPrefix adjustment
                     sourcePath =
                         pathPrefix && sourcePath.includes(pathPrefix)
@@ -184,6 +180,12 @@ async function createTsCompilerHost(options: any, svelteMap: SvelteMap) {
                                   sourcePath.indexOf(pathPrefix) + pathPrefix.length + 1
                               )
                             : sourcePath;
+                    // Due to our hack of treating .svelte files as .ts files, we need to adjust the extension
+                    if (
+                        svelteMap.get(path.join(options.rootDir, toRealSvelteFilepath(sourcePath)))
+                    ) {
+                        sourcePath = toRealSvelteFilepath(sourcePath);
+                    }
                     return `"sources":["${sourcePath}"]`;
                 });
             } else if (fileName.endsWith('js.map')) {

packages/svelte2tsx/test/emitDts/samples/typescript-declarationMap/expected/testSvelteTs.svelte.d.ts
@@ -1,0 +1,3 @@
+export declare class TestSvelteTs {
+}
+//# sourceMappingURL=testSvelteTs.svelte.d.ts.map

packages/svelte2tsx/test/emitDts/samples/typescript-declarationMap/expected/testSvelteTs.svelte.d.ts.map
@@ -1,0 +1,1 @@
+{"version":3,"file":"testSvelteTs.svelte.d.ts","sourceRoot":"","sources":["../src/testSvelteTs.svelte.ts"],"names":[],"mappings":"AAAA,qBAAa,YAAY;CAAG"}

packages/svelte2tsx/test/emitDts/samples/typescript-declarationMap/src/testSvelteTs.svelte.ts
@@ -1,0 +1,1 @@
+export class TestSvelteTs {}

commit fbff4ee34b34e9976c3de472967f854f6f27f1a4

fix: normalize paths in emitDts map
bug introduced in #2443, affecting windows systems

packages/svelte2tsx/src/emitDts.ts
@@ -288,11 +288,14 @@ async function createSvelteMap(config: EmitDtsConfig): Promise<SvelteMap> {
             version,
             noSvelteComponentTyped: noSvelteComponentTyped
         }).code;
-        svelteFiles.set(path, transformed);
+        svelteFiles.set(path.replace(/\\/g, '/'), transformed);
         return isTsFile;
     }
 
-    return { add, get: (key: string) => svelteFiles.get(key) };
+    return {
+        add,
+        get: (key: string) => svelteFiles.get(key.replace(/\\/g, '/'))
+    };
 }
 
 function isSvelteFilepath(filePath: string) {