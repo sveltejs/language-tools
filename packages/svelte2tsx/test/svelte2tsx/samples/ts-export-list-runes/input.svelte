a1c152bf90bf389a4cf8eb8d7b3a89899deb4624
paoloricciuti <ricciutipaolo@gmail.com>
2024-10-09 18:09:45 +0200

    chore: write tests

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-export-list-runes/input.svelte
@@ -0,0 +1,19 @@
+<svelte:options runes />
+<script>
+    let name1: string = "world"
+    let name2: string;
+    let name3: string = '', name4: string;
+
+    let rename1: string = '';
+    let rename2: string;
+
+    class Foo {}
+    function bar() {}
+    const baz: string = '';
+
+    class RenameFoo {}
+    function renamebar() {}
+    const renamebaz: string = '';
+
+    export { name1, name2, name3, name4, rename1 as renamed1, rename2 as renamed2, Foo, bar, baz, RenameFoo as RenamedFoo, renamebar as renamedbar, renamebaz as renamedbaz };
+</script>
