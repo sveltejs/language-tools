fe9ad7dac05efc6d5a3bc249e69ec132a91fdf04
Simon Holthausen <simon.holthausen@vercel.com>
2024-11-07 16:59:45 +0100

    chore: fix dts jsdoc test

--- packages/svelte2tsx/test/emitDts/samples/javascript-runes.v5/expected/TestRunes.svelte.d.ts
+++ packages/svelte2tsx/test/emitDts/samples/javascript-runes.v5/expected/TestRunes.svelte.d.ts
@@ -1,8 +1,17 @@
+export default TestRunes;
+type TestRunes = {
+    $on?(type: string, callback: (e: any) => void): () => void;
+    $set?(props: Partial<$$ComponentProps>): void;
+} & {
+    baz: () => void;
+};
 declare const TestRunes: import("svelte").Component<{
     foo: string;
     bar?: number;
 }, {
     baz: () => void;
 }, "bar">;
-type TestRunes = ReturnType<typeof TestRunes>;
-export default TestRunes;
+type $$ComponentProps = {
+    foo: string;
+    bar?: number;
+};
