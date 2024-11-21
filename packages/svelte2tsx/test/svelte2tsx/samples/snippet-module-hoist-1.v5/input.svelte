761eb3cae1794269e5b420935bf2818762da9f59
Simon Holthausen <simon.holthausen@vercel.com>
2024-11-21 13:20:41 +0100

    feat: hoist snippets to module context if possible
    https://github.com/sveltejs/svelte/issues/10350

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/snippet-module-hoist-1.v5/input.svelte
@@ -0,0 +1,40 @@
+<script module>
+    let module = true;
+</script>
+
+<script lang="ts">
+    import { imported } from './x';
+    let foo = true;
+</script>
+
+{#snippet hoistable1()}
+    <div>hello</div>
+{/snippet}
+
+{#snippet hoistable2(bar)}
+    <div>{bar}</div>
+{/snippet}
+
+{#snippet hoistable3(bar: string)}
+    <div>{bar}</div>
+{/snippet}
+
+{#snippet hoistable4(foo)}
+    <div>{foo}</div>
+{/snippet}
+
+{#snippet hoistable5()}
+    <button onclick={e => e}>click</button>
+{/snippet}
+
+{#snippet hoistable6()}
+    <div>{module}</div>
+{/snippet}
+
+{#snippet hoistable7()}
+    <div>{imported}</div>
+{/snippet}
+
+{#snippet not_hoistable()}
+    <div>{foo}</div>
+{/snippet}
---
+++ packages/svelte2tsx/test/svelte2tsx/samples/snippet-module-hoist-3.v5/input.svelte
@@ -0,0 +1,11 @@
+<script module lang="ts">
+    let foo = true;
+</script>
+
+{#snippet hoistable1()}
+    <div>hello</div>
+{/snippet}
+
+{#snippet hoistable2()}
+    <div>{foo}</div>
+{/snippet}
