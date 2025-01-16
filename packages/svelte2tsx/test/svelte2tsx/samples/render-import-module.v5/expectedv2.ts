3fe7d91a8e70c791634f9f5a4d90ea317a718594
paoloricciuti <ricciutipaolo@gmail.com>
2025-01-17 00:12:24 +0100

    chore: weird test

--- packages/svelte2tsx/test/svelte2tsx/samples/render-import-module.v5/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/render-import-module.v5/expectedv2.ts
@@ -3,7 +3,7 @@
 	import { render } from "render";
 ;;function render_0() {
 async () => {};
-return { props: /** @type {Record<string, never>} */ ({}), slots: {}, events: {} }}
-
-export default class Input__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_partial(__sveltets_2_with_any_event(render_0()))) {
-}
+return { props: /** @type {Record<string, never>} */ ({}), exports: {}, bindings: "", slots: {}, events: {} }}
+const Input__SvelteComponent_ = __sveltets_2_isomorphic_component(__sveltets_2_partial(__sveltets_2_with_any_event(render_0())));
+/*Ωignore_startΩ*/type Input__SvelteComponent_ = InstanceType<typeof Input__SvelteComponent_>;
+/*Ωignore_endΩ*/export default Input__SvelteComponent_;
