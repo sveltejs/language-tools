c9423822a0fa7b60e3e34da49061058d3d45ae78
Paolo Ricciuti <ricciutipaolo@gmail.com>
2025-02-25 21:47:32 +0100

    fix: properly transform `$props.id` when `$props` is assigned to `props` (#2694)
    * fix: properly transform `$props.id` when `$props` is assigned to `props`
    
    * chore: add comments

---
+++ packages/svelte2tsx/test/svelte2tsx/samples/props-variable-and-$props.id-not-$props-init.v5/expectedv2.ts
@@ -0,0 +1,13 @@
+///<reference types="svelte" />
+;function $$render() {
+
+	let props = {}/*Ωignore_startΩ*/;let $props = __sveltets_2_store_get(props);/*Ωignore_endΩ*/;
+	let id = $props.id();
+;
+async () => {
+
+id; props;};
+return { props: /** @type {Record<string, never>} */ ({}), exports: {}, bindings: "", slots: {}, events: {} }}
+const Input__SvelteComponent_ = __sveltets_2_isomorphic_component(__sveltets_2_partial(__sveltets_2_with_any_event($$render())));
+/*Ωignore_startΩ*/type Input__SvelteComponent_ = InstanceType<typeof Input__SvelteComponent_>;
+/*Ωignore_endΩ*/export default Input__SvelteComponent_;
---
+++ packages/svelte2tsx/test/svelte2tsx/samples/props-variable-and-$props.id.v5/expectedv2.ts
@@ -0,0 +1,13 @@
+///<reference types="svelte" />
+;function $$render() {
+
+	let props = $props();
+	let id = $props.id();
+;
+async () => {
+
+id; props;};
+return { props: /** @type {$$ComponentProps} */({}), exports: {}, bindings: __sveltets_$$bindings(''), slots: {}, events: {} }}
+const Input__SvelteComponent_ = __sveltets_2_fn_component($$render());
+type Input__SvelteComponent_ = ReturnType<typeof Input__SvelteComponent_>;
+export default Input__SvelteComponent_;
