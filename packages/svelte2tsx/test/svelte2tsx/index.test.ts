19307209592893b741ed2aecda85b5bcae446aeb
jycouet <jycouet@gmail.com>
2025-03-11 18:20:32 +0100

    Let just replace all tests one by one by vitest (don't skip)

---
+++ packages/svelte2tsx/test/svelte2tsx/index.test.ts
@@ -0,0 +1,12 @@
+import { svelte2tsx } from '../build';
+import { get_svelte2tsx_config, test_samples } from '../helpers';
+
+describe('svelte2tsx', () => {
+  test_samples(
+    __dirname,
+    (input, config) => {
+      return svelte2tsx(input, get_svelte2tsx_config(config, config.sampleName));
+    },
+    'ts'
+  );
+});
--- packages/svelte2tsx/vitest.config.ts
+++ packages/svelte2tsx/vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'node',
-        include: ['test/vitest/**/*.test.ts']
+        include: ['test/**/*.test.ts']
     },
     resolve: {
         alias: {
