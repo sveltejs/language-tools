75eb2bb340b8340d8e0a848d5aeb03c0f21ff4c6
Lyu, Wei-Da <36730922+jasonlyu123@users.noreply.github.com>
2025-06-19 17:02:56 +0800

    fix: key block can have its own block scope (#2768)
    #2762

--- packages/svelte2tsx/src/htmlxtojsx_v2/nodes/Key.ts
+++ packages/svelte2tsx/src/htmlxtojsx_v2/nodes/Key.ts
@@ -3,18 +3,18 @@ import { BaseNode } from '../../interfaces';
 import { isImplicitlyClosedBlock, withTrailingPropertyAccess } from '../utils/node-utils';
 
 /**
- * {#key expr}content{/key}   --->   expr; content
+ * {#key expr}content{/key}   --->   expr; {content}
  */
 export function handleKey(str: MagicString, keyBlock: BaseNode): void {
-    // {#key expr}   ->   expr;
+    // {#key expr}   ->   expr;{
     str.overwrite(keyBlock.start, keyBlock.expression.start, '', { contentOnly: true });
     const expressionEnd = withTrailingPropertyAccess(str.original, keyBlock.expression.end);
     const end = str.original.indexOf('}', expressionEnd);
-    str.overwrite(expressionEnd, end + 1, '; ');
+    str.overwrite(expressionEnd, end + 1, '; {');
 
-    // {/key}   ->
+    // {/key}   -> }
     const endKey = str.original.lastIndexOf('{', keyBlock.end - 1);
     if (!isImplicitlyClosedBlock(endKey, keyBlock)) {
-        str.overwrite(endKey, keyBlock.end, '', { contentOnly: true });
+        str.overwrite(endKey, keyBlock.end, '}', { contentOnly: true });
     }
 }
