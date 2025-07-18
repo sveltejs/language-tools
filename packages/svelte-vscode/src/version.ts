e7b06938213ccf67953744d8650a67fc1d54be74
jycouet <jycouet@gmail.com>
2025-07-18 17:53:13 +0200

    let's not redo semver!

--- packages/svelte-vscode/src/version.ts
+++ packages/svelte-vscode/src/version.ts
@@ -1,118 +1,48 @@
-import { minVersion, valid, gt, gte, lt, lte, coerce } from 'semver';
-
-type Version = {
-    isValid: boolean;
-
-    major?: number;
-    minor?: number;
-    patch?: number;
-    beta?: string;
-
-    info?: 'equal' | 'gte' | 'gt' | 'is-patch-beta';
-    specifier?: 'major' | 'minor' | 'patch';
-};
-
-export function versionSplit(str: string): Version {
-    let isValid = true;
-    let beta: string | undefined = undefined;
-    let [majorStr, minorStr, ...rest] = str?.split('.') ?? [];
-    let patchStr = rest.join('.');
-
-    let info: Version['info'] = undefined;
-    // major can have a special prefix
-    if (majorStr?.startsWith('>=')) {
-        majorStr = majorStr.slice(2);
-        info = 'gte';
-    } else if (majorStr?.startsWith('>')) {
-        majorStr = majorStr.slice(1);
-        info = 'gt';
-    } else {
-        info = 'equal';
-    }
-
-    // patch could be like: 0.22.6-exp.2 or 3.0.6-next.1
-    if (patchStr?.includes('-')) {
-        const [p1, ...rest2] = patchStr.split('-');
-        beta = rest2.join('-');
-        patchStr = p1;
-        info = 'is-patch-beta';
-    }
-
-    function toVersionNumber(val: string | undefined): number | undefined {
-        return val !== undefined && val !== '' && !isNaN(Number(val)) ? Number(val) : undefined;
-    }
-
-    const major = toVersionNumber(majorStr);
-    const minor = toVersionNumber(minorStr);
-    const patch = toVersionNumber(patchStr);
-
-    let specifier: Version['specifier'] = undefined;
-    if (patch !== undefined) {
-        specifier = 'patch';
-    } else if (minor !== undefined) {
-        specifier = 'minor';
-    } else if (major !== undefined) {
-        specifier = 'major';
-    }
-
-    if (major === undefined && minor === undefined && patch === undefined) {
-        isValid = false;
-    } else if (patch !== undefined && (minor === undefined || major === undefined)) {
-        isValid = false;
-    } else if (minor !== undefined && major === undefined) {
-        isValid = false;
-    }
-
-    return { major, minor, patch, beta, info, specifier, isValid };
-}
-
-function versionUnsupportedBelow(version_str: string, below_str: string): boolean | undefined {
-    const version = versionSplit(version_str);
-    const below = versionSplit(below_str);
-
-    if (version.major === undefined || below.major === undefined) return undefined;
-    if (version.major < below.major) return true;
-    if (version.major > below.major) return false;
-
-    if (version.minor === undefined || below.minor === undefined) {
-        if (version.major === below.major) return false;
-        else return true;
-    }
-    if (version.minor < below.minor) return true;
-    if (version.minor > below.minor) return false;
-
-    if (version.patch === undefined || below.patch === undefined) {
-        if (version.minor === below.minor) return false;
-        else return true;
-    }
-    if (version.patch < below.patch) return true;
-    if (version.patch > below.patch) return false;
-    if (version.patch === below.patch) return false;
-
-    return undefined;
-}
+import { lt, coerce, gte } from 'semver';
 
 /**
  * @example
- * const unsupported = minimumRequirement('18.3').for(process.versions.node);
+ * const supported = atLeast({
+ *     packageName: 'node',
+ *     versionMin: '18.3',
+ *     versionToCheck: process.versions.node
+ *     fallback: true // optional
+ * });
  */
-export function minimumRequirement(versionMin: string): {
-    for: (versionToCheck: string) => boolean | undefined;
-} {
-    return {
-        for: (versionToCheck: string) => {
-            if (versionToCheck === undefined) return undefined;
-            if (versionToCheck === '') return undefined;
-            try {
-                const vMin = coerce(versionMin);
-                const vToCheck = coerce(minVersion(versionToCheck));
-                if (vMin && vToCheck) {
-                    return lt(vToCheck, vMin);
-                }
-            } catch (error) {}
-            return undefined;
+export function atLeast(o: {
+    packageName: string;
+    versionMin: string;
+    versionToCheck: string;
+    fallback?: boolean;
+}): boolean | undefined {
+    const { packageName, versionMin, versionToCheck, fallback } = o;
+    if (versionToCheck === undefined || versionToCheck === '') return fallback;
 
-            return versionUnsupportedBelow(versionToCheck, versionMin);
+    if (versionToCheck.includes('catalog:') || versionToCheck.includes('http')) {
+        console.warn(`Version '${versionToCheck}' for '${packageName}' is not supported`);
+        return fallback;
+    }
+    try {
+        const vMin = coerce(versionMin);
+        const vToCheck = coerce(versionToCheck);
+        if (vMin && vToCheck) {
+            return gte(vToCheck, vMin);
         }
-    };
+    } catch (error) {}
+    return fallback;
 }
+
+const res1 = atLeast({
+    packageName: 'myPkg',
+    versionMin: '1',
+    versionToCheck: '2'
+    // fallback
+});
+//
+
+const res2 = atLeast({
+    packageName: 'myPkg',
+    versionMin: '1',
+    versionToCheck: '2',
+    fallback: true
+});
--- packages/svelte-vscode/test/version.spec.ts
+++ packages/svelte-vscode/test/version.spec.ts
@@ -1,158 +1,30 @@
 import { expect, describe, it } from 'vitest';
-import { versionSplit, minimumRequirement } from '../src/version';
+import { atLeast } from '../src/version';
 
-describe('versionSplit', () => {
-    const combinationsVersionSplit: {
-        version: string;
-        focus?: keyof ReturnType<typeof versionSplit>;
-        expected: ReturnType<typeof versionSplit>;
-    }[] = [
-        {
-            version: '18.13.0',
-            expected: {
-                major: 18,
-                minor: 13,
-                patch: 0,
-                info: 'equal',
-                specifier: 'patch',
-                isValid: true
-            }
-        },
-        {
-            version: 'x.13.0',
-            focus: 'isValid',
-            expected: { isValid: false }
-        },
-        {
-            version: '18.y.0',
-            focus: 'isValid',
-            expected: { isValid: false }
-        },
-        {
-            version: '18.13.z',
-            expected: {
-                major: 18,
-                minor: 13,
-                info: 'equal',
-                specifier: 'minor',
-                isValid: true
-            }
-        },
-        {
-            version: '18',
-            expected: {
-                major: 18,
-                info: 'equal',
-                specifier: 'major',
-                isValid: true
-            }
-        },
-        {
-            version: '18.13',
-            expected: {
-                major: 18,
-                minor: 13,
-                info: 'equal',
-                specifier: 'minor',
-                isValid: true
-            }
-        },
-        {
-            version: 'invalid',
-            focus: 'isValid',
-            expected: { isValid: false }
-        },
-        {
-            version: '>=18',
-            expected: {
-                major: 18,
-                info: 'gte',
-                specifier: 'major',
-                isValid: true
-            }
-        },
-        {
-            version: '>=18.7',
-            expected: {
-                major: 18,
-                minor: 7,
-                info: 'gte',
-                specifier: 'minor',
-                isValid: true
-            }
-        },
-        {
-            version: '>18',
-            expected: {
-                major: 18,
-                info: 'gt',
-                specifier: 'major',
-                isValid: true
-            }
-        },
-        {
-            version: '>18.7',
-            expected: {
-                major: 18,
-                minor: 7,
-                info: 'gt',
-                specifier: 'minor',
-                isValid: true
-            }
-        },
-        {
-            version: '1.1.1-beta.1',
-            expected: {
-                major: 1,
-                minor: 1,
-                patch: 1,
-                beta: 'beta.1',
-                info: 'is-patch-beta',
-                specifier: 'patch',
-                isValid: true
-            }
-        }
+describe.only('atLeast', () => {
+    const combinationsAtLeast = [
+        { min: '5', version: '>=5', supported: true },
+        { min: '5', version: '>=5.0.0', supported: true },
+        { min: '5', version: '5.0.0', supported: true },
+        { min: '5', version: '5', supported: true },
+        { min: '5', version: '4', supported: false },
+        { min: '5', version: '4.9', supported: false },
+        { min: '5', version: '', supported: undefined },
+        { min: '5', version: 'catalog:', supported: undefined },
+        { min: '5', version: 'latest', supported: undefined },
+        { min: '5', version: 'latest', supported: true, fallback: true }
     ];
-    it.each(combinationsVersionSplit)('should handle $version', ({ version, expected, focus }) => {
-        const res = versionSplit(version);
-        if (focus) {
-            expect(res[focus]).toEqual(expected[focus]);
-        } else {
-            expect(res).toEqual(expected);
-        }
-    });
-});
-
-describe.only('minimumRequirement', () => {
-    const combinationsMinimumRequirement = [
-        // strict
-        { min: '18.3.0', version: '17', unsupported: true },
-        { min: '18.3.0', version: '18.2', unsupported: true },
-        { min: '18.3.1', version: '18.3.0', unsupported: true },
-        { min: '18.3.0', version: '18.3.1', unsupported: false },
-        { min: '18.3.0', version: '18.3.0', unsupported: false },
-        { min: '18.3', version: '18.3.0', unsupported: false },
-        { min: '18.3', version: '18.3.1', unsupported: false },
-        { min: '18', version: '18.3.1', unsupported: false },
-        { min: '18', version: '18', unsupported: false },
-        { min: 'b', version: 'a', unsupported: undefined },
-        { min: '18.3', version: '18.3', unsupported: false },
-        { min: '18.3', version: '18.4', unsupported: false },
-        { min: '18.3', version: '18.2', unsupported: true },
-
-        // if it's undefined, we can't say anything...
-        { min: '18.3', version: undefined!, unsupported: undefined },
-        { min: '18.3', version: '', unsupported: undefined },
-
-        // more fun stuff
-        { min: '7', version: 'latest', unsupported: undefined },
-        { min: '7', version: '>=7', unsupported: false },
-        { min: '7', version: '>7', unsupported: true }
-    ] as const;
-    it.each(combinationsMinimumRequirement)(
-        '(min $min, version $version) => unsupported: $unsupported',
-        ({ min, version, unsupported }) => {
-            expect(minimumRequirement(min).for(version)).toEqual(unsupported);
+    it.each(combinationsAtLeast)(
+        '(min $min, version $version) => supported: $supported',
+        ({ min, version, supported, fallback }) => {
+            expect(
+                atLeast({
+                    packageName: 'myPkg',
+                    versionMin: min,
+                    versionToCheck: version,
+                    fallback
+                })
+            ).toEqual(supported);
         }
     );
 });
