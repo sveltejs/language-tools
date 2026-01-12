24db72c0fcd69b6e8b6e8f07da8930fa52d4a225
abdel-17 <abdelrahmanbahaa@proton.me>
2026-01-12 07:00:21 +0200

    Revert "fix: failing test"
    This reverts commit 2a08b55f0f8aec1faee3450b27108cee4dce0c02.

--- packages/language-server/src/lib/documents/parseHtml.ts
+++ packages/language-server/src/lib/documents/parseHtml.ts
@@ -37,14 +37,19 @@ function preprocess(text: string) {
     let scanner = createScanner(text);
     let token = scanner.scan();
     let currentStartTagStart: number | null = null;
+    let moustacheCheckStart = 0;
+    let moustacheCheckEnd = 0;
+    let lastToken = token;
 
     while (token !== TokenType.EOS) {
         const offset = scanner.getTokenOffset();
+        let blanked = false;
 
         switch (token) {
             case TokenType.StartTagOpen:
                 if (shouldBlankStartOrEndTagLike(offset)) {
                     blankStartOrEndTagLike(offset);
+                    blanked = true;
                 } else {
                     currentStartTagStart = offset;
                 }
@@ -53,6 +58,7 @@ function preprocess(text: string) {
             case TokenType.StartTagClose:
                 if (shouldBlankStartOrEndTagLike(offset)) {
                     blankStartOrEndTagLike(offset);
+                    blanked = true;
                 } else {
                     currentStartTagStart = null;
                 }
@@ -71,13 +77,23 @@ function preprocess(text: string) {
                     shouldBlankStartOrEndTagLike(offset)
                 ) {
                     blankStartOrEndTagLike(offset);
+                    blanked = true;
                 }
                 break;
 
-            case TokenType.Content:
+            case TokenType.Content: {
+                moustacheCheckEnd = scanner.getTokenEnd();
+                if (token !== lastToken) {
+                    moustacheCheckStart = offset;
+                }
                 break;
+            }
         }
 
+        // blanked, so the token type is invalid
+        if (!blanked) {
+            lastToken = token;
+        }
         token = scanner.scan();
     }
 
@@ -87,7 +103,21 @@ function preprocess(text: string) {
         if (currentStartTagStart != null) {
             return isInsideMoustacheTag(text, currentStartTagStart, offset);
         }
-        return isInsideMoustacheTag(text, null, offset);
+
+        const index = text
+            .substring(moustacheCheckStart, moustacheCheckEnd)
+            .lastIndexOf('{', offset);
+
+        const lastMustacheTagStart = index === -1 ? null : moustacheCheckStart + index;
+        if (lastMustacheTagStart == null) {
+            return false;
+        }
+
+        return isInsideMoustacheTag(
+            text.substring(lastMustacheTagStart),
+            null,
+            offset - lastMustacheTagStart
+        );
     }
 
     function blankStartOrEndTagLike(offset: number) {
--- packages/language-server/src/lib/documents/utils.ts
+++ packages/language-server/src/lib/documents/utils.ts
@@ -442,34 +442,27 @@ export function getLangAttribute(...tags: Array<TagInformation | null>): string 
 
 /**
  * Checks whether given position is inside a moustache tag (which includes control flow tags)
- * using a simple bracket matching heuristic which can fail for braces inside strings/comments.
+ * using a simple bracket matching heuristic which might fail under conditions like
+ * `{#if {a: true}.a}`
  */
 export function isInsideMoustacheTag(html: string, tagStart: number | null, position: number) {
     if (tagStart === null) {
         // Not inside <tag ... >
         const charactersBeforePosition = html.substring(0, position);
-        tagStart = Math.max(
-            // TODO: make this just check for '{'?
-            // Theoretically, someone could do {a < b} in a simple moustache tag
-            charactersBeforePosition.lastIndexOf('{#'),
-            charactersBeforePosition.lastIndexOf('{:'),
-            charactersBeforePosition.lastIndexOf('{@')
+        return (
+            Math.max(
+                // TODO make this just check for '{'?
+                // Theoretically, someone could do {a < b} in a simple moustache tag
+                charactersBeforePosition.lastIndexOf('{#'),
+                charactersBeforePosition.lastIndexOf('{:'),
+                charactersBeforePosition.lastIndexOf('{@')
+            ) > charactersBeforePosition.lastIndexOf('}')
         );
-        if (tagStart === -1) {
-            return false;
-        }
+    } else {
+        // Inside <tag ... >
+        const charactersInNode = html.substring(tagStart, position);
+        return charactersInNode.lastIndexOf('{') > charactersInNode.lastIndexOf('}');
     }
-
-    let depth = 0;
-    for (let i = tagStart; i < position; i++) {
-        const char = html[i];
-        if (char === '{') {
-            depth++;
-        } else if (char === '}' && depth > 0) {
-            depth--;
-        }
-    }
-    return depth > 0;
 }
 
 export function inStyleOrScript(document: Document, position: Position) {
