e2f09eb1379a08983c48518e7af65a49736fa813
Lyu, Wei-Da <36730922+jasonlyu123@users.noreply.github.com>
2026-01-31 14:58:54 +0800

    fix: don't hoist type/snippet referencing $store (#2926)
    * fix: don't hoist type/snippet referencing $store
    
    * svelte-check changeset

---
+++ .changeset/hip-lands-joke.md
@@ -0,0 +1,6 @@
+---
+'svelte2tsx': patch
+'svelte-check': patch
+---
+
+fix: don't hoist type/snippet referencing $store
---
+++ packages/svelte2tsx/test/svelte2tsx/samples/snippet-module-hoist-7.v5/input.svelte
@@ -0,0 +1,15 @@
+<script module>
+    import { store2 } from './foo';
+</script>
+
+<script>
+    import { store } from './foo';
+</script>
+
+{#snippet _foo()}
+    {$store}
+{/snippet}
+
+{#snippet _foo2()}
+    {$store2}
+{/snippet}
