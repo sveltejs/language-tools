5b90ac5a935f673494878cae42ca3a6a1a1a5daf
Simon Holthausen <simon.holthausen@vercel.com>
2026-02-26 15:38:25 +0100

    fix: more robust detection of `lang="ts"` attribute
    Fixes #2956

---
+++ packages/svelte-check/test-success/GenericsWithAngleBrackets.svelte
@@ -0,0 +1,12 @@
+<script
+    generics="T extends Record<string, unknown> & { id: string }"
+    lang="ts"
+>
+    import type { Readable } from 'svelte/store';
+
+    export let store: Readable<T[]>;
+</script>
+
+{#if store}
+    <p>ok</p>
+{/if}
