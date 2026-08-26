40da59ded0e8894d56c69a6a15b46ec0c396e017
Cole Crouter <cole.crouter@vaderstad.com>
2026-08-26 13:35:56 -0600

    fix: hide generated snippet document symbols

---
+++ packages/language-server/test/plugins/typescript/testfiles/documentsymbols-snippets.v5/input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    const value = true;
+    const items = [1, 2, 3];
+</script>
+
+{items.map((item) => item)}
+
+{#each items as item}
+    {@const getter = () => item}
+    {getter()}
+{/each}
+
+<Component>
+    {#snippet children()}
+        <Child {value} />
+    {/snippet}
+</Component>
+
+<Component>
+    {#snippet children({ data })}
+        <Child {data} />
+    {/snippet}
+</Component>
