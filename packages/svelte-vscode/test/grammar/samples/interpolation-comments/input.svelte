2cfcc15b4c44dfc1128432e20ca663fe20bdd12e
Cole Crouter <colecrouter@hotmail.com>
2026-09-02 19:32:15 -0600

    fix: highlight comments on first line of template expressions (#3116)

---
+++ packages/svelte-vscode/test/grammar/samples/interpolation-comments/input.svelte
@@ -0,0 +1,10 @@
+{// A leading line comment formatted next to the opening brace
+value ? 'yes' : 'no'}
+
+{
+    // A leading line comment on the following line
+    value ? 'yes' : 'no'}
+
+{/* A leading block comment */ value ? 'yes' : 'no'}
+
+{/
