8794269d2701ea3d819fdf6961fcdd69b16ce855
Lyu, Wei Da <jason.lyu@supergeo.com.tw>
2026-09-11 15:03:47 +0800

    fix svelte 4 test

--- packages/svelte2tsx/test/svelte2tsx/samples/non-import-between-imports/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/non-import-between-imports/expectedv2.ts
@@ -8,7 +8,7 @@ function $$render() {
     console.error('hi')
 ;
 async () => {};
-return { props: /** @type {Record<string, never>} */ ({}), exports: {}, bindings: "", slots: {}, events: {} }}
-const Input__SvelteComponent_ = __sveltets_2_isomorphic_component(__sveltets_2_partial(__sveltets_2_with_any_event($$render())));
-/*Ωignore_startΩ*/type Input__SvelteComponent_ = InstanceType<typeof Input__SvelteComponent_>;
-/*Ωignore_endΩ*/export default Input__SvelteComponent_;
+return { props: /** @type {Record<string, never>} */ ({}), slots: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_partial(__sveltets_2_with_any_event($$render()))) {
+}
--- packages/svelte2tsx/test/svelte2tsx/samples/ts-check/expectedv2.ts
+++ packages/svelte2tsx/test/svelte2tsx/samples/ts-check/expectedv2.ts
@@ -13,7 +13,7 @@
     //    @ts-check
 ;
 async () => {};
-return { props: {} as Record<string, never>, exports: {}, bindings: "", slots: {}, events: {} }}
-const Input__SvelteComponent_ = __sveltets_2_isomorphic_component(__sveltets_2_with_any_event($$render()));
-/*Ωignore_startΩ*/type Input__SvelteComponent_ = InstanceType<typeof Input__SvelteComponent_>;
-/*Ωignore_endΩ*/export default Input__SvelteComponent_;
+return { props: {} as Record<string, never>, slots: {}, events: {} }}
+
+export default class Input__SvelteComponent_ extends __sveltets_2_createSvelte2TsxComponent(__sveltets_2_with_any_event($$render())) {
+}
